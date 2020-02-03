5d2011e8a44fb88a1eec2947dd4eddd4f063fd16
--1stRayos <--emc1140@hotmail.com>
2020-02-03 09:45:13 -0600

    Weekly tweaks & fixes

--- Controller.py
+++ Controller.py
@@ -3,19 +3,19 @@ from Webscraping import flickr, foundry, furaffinity, gelbooru, sankaku, posespa
 
 def erotica3():
 
-    # process = subprocess.Popen([
-        # r'Webscraping\PixivUtil\PixivUtil2.exe',
-        # '-s', '6', 'y', '0', '7', '-x'
-        # #              start end  stop
-        # ])
+    process = subprocess.Popen([
+        r'Webscraping\PixivUtil\PixivUtil2.exe',
+        '-s', '6', 'y', '0', '7', '-x'
+        #              start end  stop
+        ])
     threads = [
-        # threading.Thread(target=webscrapers),
-        # threading.Thread(target=process.wait)
+        threading.Thread(target=webscrapers),
+        threading.Thread(target=process.wait)
         ]
     for thread in threads: thread.start()
     for thread in threads: thread.join()
     
-    # favorites.setup()
+    favorites.setup()
     
     threads = [
         threading.Thread(target=gelbooru.setup),
@@ -37,7 +37,7 @@ def webscrapers():
         print(f'Twitter: {error}')
 
 threads = [
-    # threading.Thread(target=flickr.setup),
+    threading.Thread(target=flickr.setup),
     threading.Thread(target=erotica3),
     # threading.Thread(target=posespace.setup)
     ]
