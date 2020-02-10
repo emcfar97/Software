4ae0d824228e55d95f8efefc7227e66906874625
--1stRayos <--emc1140@hotmail.com>
2020-02-09 23:54:01 -0600

    Weekly tweaks and fixes

--- Webscraping/posespace.py
+++ Webscraping/posespace.py
@@ -1,7 +1,8 @@
-from selenium.webdriver.common.action_chains import ActionChains
+import imageio, cv2, hashlib
+import numpy as np
 from selenium.common.exceptions import WebDriverException
 import mysql.connector as sql
-from .utils import *
+from os.path import join
 
 DATAB = sql.connect(
     user='root', password='SchooL1@', database='userData', 
@@ -9,6 +10,7 @@ DATAB = sql.connect(
     )
 CURSOR = DATAB.cursor() 
 SITE = 'posespace'
+TYPE = 'Erotica 2'
 
 def initialize(driver, url='/posetool/favs.aspx'):
 
@@ -19,7 +21,7 @@ def initialize(driver, url='/posetool/favs.aspx'):
     while True:
         try:
             hrefs = [
-                (*href, SITE) for href in {(target.get('href'),) 
+                (*href, SITE) for href in {(target.text,) 
                 for target in html.findAll(class_='emph')} - query
                 ]
             break
@@ -34,12 +36,13 @@ def page_handler(driver, hrefs):
     
     if not hrefs: return
     size = len(hrefs)
+    hasher = hashlib.md5()
     url = 'https://www.posespace.com/img/contact/'
 
     for num, (href,) in enumerate(hrefs):
         progress(size, num, SITE)
+        model = href[:-2]
 
-        name = join(PATH, 'Images', SITE, f'{href}.gif')
         href_a = f'{url}{href}contacta.jpg'
         href_b = f'{url}{href}contactb.jpg'
 
@@ -49,12 +52,15 @@ def page_handler(driver, hrefs):
         image = Image.new('RGB', (image_a.width, image_a.height+image_b.height))
         image.paste(image_a)
         image.paste(image_b, (0, image_a.height))
-        image.show()
+
+        name = join(r'C:\Users\Emc11\Downloads\test', f'{href}.gif')
+        image.save(name)
+        # name = join(PATH, 'エラティカ ニ', f'{hasher.hexdigest()}.gif')
 
         continue
         while True:
             try:
-                CURSOR.execute(UPDATE[3], (name, hash, image, href))
+                CURSOR.execute(UPDATE[3], (name, model, hash, image, href))
                 DATAB.commit()
                 break
             except sql.errors.OperationalError: continue
@@ -64,6 +70,65 @@ def page_handler(driver, hrefs):
                 break
         
     progress(size, size, SITE)
+
+def make_gif(path, name, error=False):
+
+    image = Image.open(join(path, f'{name}.jpg'))
+    corners = find_corners(join(path, f'{name}.jpg'))
+    images = [
+        image.crop((*corner[0], *corner[1]))
+        for corner in zip(corners[0][:24], corners[1][:24])
+        ]
+    os.mkdir(join(path[:-6], name))
+    for num, image in enumerate(images, 1):
+        image.save(join(path[:-6], name, f'{name}_{num:02d}.jpg'))
+    imageio.mimsave(join(path[:-6], f'{name}.gif'), images, duration=.20)
+    
+def find_corners(path):
+
+    def find_row(row):
+        
+        above = gray.shape[1] - np.count_nonzero(gray[row-1])
+        center= gray.shape[1] - np.count_nonzero(gray[row])
+        below = gray.shape[1] - np.count_nonzero(gray[row+1])
+        
+        return (
+            above<center and above<below and below!=0 and above<1,
+            below<center and below<above and above!=0 and below<1
+            )
+
+    def find_col(row, col, span=20):
+
+        rght = 2*span - np.count_nonzero(gray[row-span:row+span,col-1])
+        midd = 2*span - np.count_nonzero(gray[row-span:row+span,col])
+        left = 2*span - np.count_nonzero(gray[row-span:row+span,col+1])
+        
+        return (
+            rght<midd and rght<left and left!=0 and rght<=1,
+            left<midd and left<rght and rght!=0 and left<=1
+            )
+
+    corners = [], []
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.adaptiveThreshold(
+        gray, 255,
+        cv2.ADAPTIVE_THRESH_MEAN_C, 
+        cv2.THRESH_BINARY, 21, 4
+        )
+     
+    for row in range(1, gray.shape[0]-1):
+
+        top, bot = find_row(row)
+        if top or bot:
+            
+            for col in range(1, gray.shape[1]-1):
+              
+                rght, left = find_col(row, col)
+                if rght and top: corners[0].append([col, row])
+                elif left and bot: corners[1].append([col, row])
+        
+    return corners
 
 def setup(initial=True):
     
@@ -80,8 +145,15 @@ def setup(initial=True):
 
 if __name__ == '__main__':
 
-    driver = get_driver(headless=True)
-    login(driver, SITE)
-    initialize(driver)
+    from utils import *
+    driver = get_driver(True)
+    # login(driver, SITE)
+    # initialize(driver)
+    
+    CURSOR.execute(SELECT[2], (SITE,))
+    page_handler(driver, CURSOR.fetchall())
+
     driver.close()
     DATAB.close()
+
+else: from .utils import *
