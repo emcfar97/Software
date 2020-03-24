79cac9b1934aad3a7928bcc7622f6a5bcbfac1c9
Ethan McFarland <emc1140@hotmail.com>
2020-03-24 17:29:14 -0500

    .

--- CodeKC/djangoProject/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\emc11\dropbox\software\codekc\djangoproject\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
