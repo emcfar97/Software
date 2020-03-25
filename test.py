5260475f8e54f0ebf641bc7e9896ebc790b3b2f7
--1stRayos <--emc1140@hotmail.com>
2020-03-25 00:58:42 -0500

    .

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
-#
-    # import tensorflow as tf
-    # from tensorflow import keras
-    # from tensorflow.keras import Model, Input, layers
-
-    # inputs = Input(shape=(512,), name='input')
-    # x = layers.Dense(64, activation='relu', name='dense_1')(inputs)
-    # x = layers.Dense(64, activation='relu', name='dense_2')(x)
-    # outputs = layers.Dense(3, activation='softmax', name='predictions')(x)
-
-    # model = Model(inputs=inputs, outputs=outputs, name='medium layer')
-    # model.compile(
-    #     optimizer='adam', metrics=['accuracy'],
-    #     loss='sparse_categorical_crossentropy'
-    #     )
-    # model.save(r'Machine Learning\Master\medium.h5')
-
 import os, shutil
 from os.path import splitext, join, exists
 import mysql.connector as sql
@@ -72,3 +55,47 @@ sprites = {}
     #                 char = 'rin'
                 
     #             sprites[char][pose].append(expr)
+
+# Houses
+# 1 2 3 4 5
+
+from constraint import *
+problem = Problem()
+
+nationality = ["English", "Spanish", "Ukrainian", "Norwegian", "Japanese"]
+pet = ["dog", "snails", "fox", "horse", "zebra"]
+cigarette = ["Old Gold", "Kools", 
+"Chesterfields", "Lucky Strike", "Parliaments"]
+colour = ["red", "green", "yellow", "blue", "ivory"]
+beverage = ["coffee", "milk", "orange juice", "water", "tea"]
+
+criteria = nationality + pet + cigarette + colour + beverage
+problem.addVariables(criteria,[1,2,3,4,5])
+
+problem.addConstraint(AllDifferentConstraint(), nationality)
+problem.addConstraint(AllDifferentConstraint(), pet)
+problem.addConstraint(AllDifferentConstraint(), cigarette)
+problem.addConstraint(AllDifferentConstraint(), colour)
+problem.addConstraint(AllDifferentConstraint(), beverage)
+
+problem.addConstraint(lambda e, r: e == r, ["English","red"])
+problem.addConstraint(lambda s, d: s == d, ("Spanish","dog"))
+problem.addConstraint(lambda c, g: c == g, ("coffee","green"))
+problem.addConstraint(lambda u, t: u == t, ("Ukrainian","tea"))
+problem.addConstraint(lambda g, i: g-i == 1, ("green","ivory"))
+problem.addConstraint(lambda o, s: o == s, ("Old Gold","snails"))
+problem.addConstraint(lambda k, y: k == y, ("Kools","yellow"))
+problem.addConstraint(InSetConstraint([3]), ["milk"])
+problem.addConstraint(InSetConstraint([1]), ["Norwegian"])
+problem.addConstraint(lambda c, f: abs(c-f) == 1, ("Chesterfields","fox"))
+problem.addConstraint(lambda k, h: abs(k-h) == 1, ("Kools","horse"))
+problem.addConstraint(lambda l, o: l == o, ["Lucky Strike","orange juice"])
+problem.addConstraint(lambda j, p: j == p, ["Japanese","Parliaments"])
+problem.addConstraint(lambda k, h: abs(k-h) == 1, ("Norwegian","blue"))
+
+solution = problem.getSolutions()[0]
+
+for i in range(1,6):
+    for x in solution:
+        if solution[x] == i:
+            print str(i), x
