8be9b9dd35736807ba86b2eede72457caa55f527
--1stRayos <--emc1140@hotmail.com>
2020-05-29 01:24:08 -0500

    Fixed f-strings

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -5,21 +5,17 @@ from PyQt5.QtGui import QImage, QPixmap, QMovie
 from datetime import date
 from cv2 import VideoCapture
 import mysql.connector as sql
+from os import getcwd
 
+ROOT = getcwd()[:2].upper()
 DATAB = sql.connect(
     user='root', password='SchooL1@', database='userData', 
     host='192.168.1.43' if __file__.startswith(('e:\\', 'e:/')) else '127.0.0.1'
     )
 CURSOR = DATAB.cursor(buffered=True)
-BASE = 'SELECT path, tags, artist, stars, rating, type FROM imageData'
-UPDATE = f'UPDATE imageData SET date_used="{date.today()}" WHERE path=%s'
-MODIFY = 'UPDATE imageData SET {} WHERE path=%s'
-DELETE = 'DELETE FROM imageData WHERE path=%s'
-NEZUMI = r'C:\Program Files (x86)\Lazy Nezumi Pro\LazyNezumiPro.exe'
-
-if __file__.startswith(('e:\\','e:/')):
-
-    BASE = BASE.replace('path','REPLACE(path, "C:", "E:")')
-    UPDATE = UPDATE.replace('%s', "REPLACE(%s, 'E:', 'C:')")
-    MODIFY = MODIFY.replace('%s', "REPLACE(%s, 'E:', 'C:')")
-    DELETE = DELETE.replace('%s', "REPLACE(%s, 'E:', 'C:')")
+BASE = f'SELECT REPLACE(path, "C:", "{ROOT}"), tags, artist, stars, rating, type FROM imageData'
+COMIC = 'SELECT path FROM comics'
+UPDATE = f'UPDATE imageData SET date_used="{date.today()}" WHERE path=REPLACE(%s, "{ROOT}", "C:")'
+MODIFY = f'UPDATE imageData SET {{}} WHERE path=REPLACE(%s, "{ROOT}", "C:")'
+DELETE = f'DELETE FROM imageData WHERE path=REPLACE(%s, "{ROOT}", "C:")'
+NEZUMI = rf'{ROOT}:\Program Files (x86)\Lazy Nezumi Pro\LazyNezumiPro.exe'
