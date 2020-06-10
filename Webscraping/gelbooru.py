1686facb0ba2ff1c8be2937b4468a4cc36da50bb
Ethan McFarland <emc1140@hotmail.com>
2020-06-10 00:12:28 -0500

    Returned to selenium webscraping

--- Webscraping/gelbooru.py
+++ Webscraping/gelbooru.py
@@ -3,7 +3,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 SITE = 'gelbooru'
     
-def initialize(url='?page=favorites&s=view&id=173770&pid=0', query=0):
+def initialize(driver, url='?page=favorites&s=view&id=173770&pid=0', query=0):
     
     def next_page(pages):
         
@@ -13,9 +13,9 @@ def initialize(url='?page=favorites&s=view&id=173770&pid=0', query=0):
     if not query:
         execute(SELECT[0], (SITE,))
         query = CURSOR.fetchall()
-    page_source = requests.get(f'https://gelbooru.com/index.php{url}').content
-    html = bs4.BeautifulSoup(page_source, 'lxml')
-    
+    driver.get(f'https://gelbooru.com/index.php{url}')
+    html = bs4.BeautifulSoup(driver.page_source, 'lxml')
+
     hrefs = [
         (target.get('href'), 1, SITE) for target in 
         html.findAll('a', id=re.compile(r'p\d+'), href=True)
@@ -24,11 +24,11 @@ def initialize(url='?page=favorites&s=view&id=173770&pid=0', query=0):
     execute(INSERT[0], hrefs, 1)
         
     next = next_page(html.find(id='paginator').contents)   
-    if hrefs and next: initialize(next, query)
+    if hrefs and next: initialize(driver, next, query)
     
     DATAB.commit()
 
-def page_handler(hrefs):
+def page_handler(driver, hrefs):
 
     if not hrefs: return
     size = len(hrefs)
@@ -36,8 +36,8 @@ def page_handler(hrefs):
     for num, (href,) in enumerate(hrefs):
 
         progress(size, num, SITE)
-        page_source = requests.get(f'https://gelbooru.com/{href}').content
-        html = bs4.BeautifulSoup(page_source, 'lxml')
+        driver.get(f'https://gelbooru.com/{href}')
+        html = bs4.BeautifulSoup(driver.page_source, 'lxml')
 
         metadata = ' '.join(
             '_'.join(tag.text.split(' ')[1:-1]) for tag in 
@@ -55,11 +55,9 @@ def page_handler(hrefs):
             tags, metadata, True, artists, True
             )
         
-        try:
-            image = html.find(
-            src=re.compile('https://img\d.gelbooru.com/i.+')
-            ).get('src')
-        except: image = handle_error(href)
+        image = driver.find_element_by_link_text(
+            'Original image'
+            ).get_attribute('href')
         name = get_name(image.split('/')[-1], 1)
         save_image(name, image, exif)
         hash_ = get_hash(name)
@@ -80,23 +78,14 @@ def page_handler(hrefs):
 
     progress(size, size, SITE)
 
-def handle_error(href):
-
-    driver = get_driver(True)
-    driver.get(f'https://gelbooru.com/{href}')
-    src = driver.find_element_by_link_text(
-        'Original image'
-        ).get_attribute('href')
-    driver.close()
-
-    return src
-
 def setup(initial=True):
     
     try:
-        if initial: initialize()
+        driver = get_driver(headless=0)
+        login(driver, SITE)
+        if initial: initialize(driver)
         CURSOR.execute(SELECT[2],(SITE,))
-        page_handler(CURSOR.fetchall())
+        page_handler(driver, CURSOR.fetchall())
     except Exception as error: print(f'{SITE}: {error}')
 
 if __name__ == '__main__':
