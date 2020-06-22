c815fe2816fd41aa71b0880b00759610eed20011
--1stRayos <--emc1140@hotmail.com>
2020-06-21 21:32:04 -0500

    Added shadow to text, tweaks & fixes

--- ffmpegScripts.py
+++ ffmpegScripts.py
@@ -1,26 +1,25 @@
 import ffmpeg
 from ffprobe import FFProbe
+from os.path import join, splitext, split, isfile
 from os import remove, listdir, getcwd
-from os.path import join, exists, splitext
 
-def get_stream(folder, files, text=0):
-    
+def get_stream(files, text=0):
+
     if text:
         stream = [
-            ffmpeg.input(join(folder, file)).drawtext(
-                text=splitext(file)[0], fontsize=42, 
-                x=int(FFProbe(join(folder, file)).streams[0].width) * .75, 
-                y=int(FFProbe(join(folder, file)).streams[0].height) * .90
+            ffmpeg.input(file).drawtext(
+                text=splitext(split(files[0])[-1])[0], fontsize=45, 
+                x=int(FFProbe(file).streams[0].width) * .70, 
+                y=int(FFProbe(file).streams[0].height) * .85,
+                shadowcolor='white', shadowx=2, shadowy=2
                 ) 
             for file in files
             ]
     else:
-        stream = [
-            ffmpeg.input(join(folder, file)) for file in files
-            ]
-    new = join(dest, files[0]).replace(splitext(files[0])[-1], '.mp4')
+        stream = [ffmpeg.input(file) for file in files]
+    new = join(dest, split(files[0])[-1]).replace(splitext(files[0])[-1], '.mp4')
     
-    return stream, new
+    return new, stream
 
 def get_folders():
     
@@ -35,7 +34,6 @@ def get_folders():
 ROOT = getcwd()[:2].upper()
 source = rf'{ROOT}\Users\Emc11\Videos\Captures'
 dest = rf'{ROOT}\Users\Emc11\Dropbox\Videos\Captures'
-EXT = 'flv', 'mkv'
 
 while True:
     user_input = input(
@@ -48,20 +46,21 @@ while True:
 
             files = [
                 (
-                    join(source, file), splitext(file)[0], 
+                    join(source, file),
                     join(dest, file.replace(splitext(file)[1], '.mp4'))
                     )
-                for file in listdir(source) if file.endswith(EXT)
+                for file in listdir(source) if isfile(file)
                 ]
             
-            for file, title, mp4 in files:
+            for file, mp4 in files:
                 try: 
                     if text:
                         metadata = FFProbe(file).streams[0]
                         ffmpeg.input(file).drawtext(
-                            text=title, fontsize=42, 
-                            x=int(metadata.width) * .75, 
-                            y=int(metadata.height) * .90
+                            text=splitext(split(file)[-1])[0], fontsize=45, 
+                            x=int(metadata.width) * .70, 
+                            y=int(metadata.height) * .85,
+                            shadowcolor='white', shadowx=2, shadowy=2
                         ).output(mp4, crf=20, preset='fast').run()
                     else: 
                         ffmpeg.input(file).output(mp4, crf=20, preset='fast').run()
@@ -77,23 +76,26 @@ while True:
                 
                 if not folder.endswith((targets)): continue
                 folder = join(source, folder)
-                stream, new = get_stream(folder, listdir(folder), text)
+                files = [join(folder, file) for file in listdir(folder)]
+                new, stream = get_stream(files, text)
                 
                 try: ffmpeg.concat(*stream).output(new, crf=20, preset='fast').run()
                 except Exception as error: print(error); continue
-                for file in stream: remove(file.node.kwargs['filename'])
+                for file in files: remove(file)
 
         elif user_input == '3': # change framerate
             
+            text = 1 if input('Overlay text? ').lower() in 'yes' else 0
             targets = get_folders()
 
             for folder in listdir(source):
 
                 if not folder.endswith((targets)): continue
                 folder = join(source, folder)
-                stream, new = get_stream(folder)
+                files = [join(folder, file) for file in listdir(folder)]
+                new, stream = get_stream(files, text)
                 
-                desired = int(input('Enter desired length (minutes): ')) * 60
+                desired = float(input('Enter desired length (minutes): ')) * 60
                 duration = sum(
                     float(FFProbe(join(folder, file)).streams[0].duration)
                     for file in listdir(folder)
@@ -101,27 +103,27 @@ while True:
                 try:
                     ffmpeg.concat(*stream).setpts(
                         f'{desired / duration:.4f}*PTS') \
-                        .output(new, crf=20, preset='fast').run()
+                        .output(new, crf=20, preset='fast'
+                        ).run()
                 except Exception as error: print(error); continue
-                for file in stream: remove(file.node.kwargs['filename'])
+                for file in files: remove(file)
                 
         elif user_input == '4': # download m3u8
             
             url = input('Enter url: ')
             name = f'{url.split("/")[3]}.mp4'
             ffmpeg.input(url).output(
-                rf'{ROOT}\Users\Emc11\Downloads\{name}').run()
+                rf'{ROOT}\Users\Emc11\Downloads\Images\{name}'
+                ).run()
             
-        elif user_input == '5':
+        elif user_input == '5': # check directories
         
             for file in listdir(source):
-                if file.endswith('.ini'): continue
                 if file.startswith('Batch'):
                     print(f'{file}: {listdir(join(source, file))}')
-                    continue
-                print(file)
+                elif not file.endswith('.ini'): print(file)
             print() 
         
         elif user_input == '6': break
             
-    except: continue
+    except Exception as error: print(error, '\n')
