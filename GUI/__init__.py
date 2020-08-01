65a31489f960e370e4e81196724af4ecca096091
Ethan McFarland <emc1140@hotmail.com>
2020-07-31 23:22:14 -0500

    Added class construct for sql, streamlined imports

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,18 +1,48 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QStackedWidget, QTableView, QLabel, QLabel, QFormLayout, QLineEdit, QRadioButton, QComboBox, QMessageBox, QDesktopWidget, QStatusBar
-from PyQt5.QtCore import Qt, QTimer, QSize, QUrl
-from PyQt5.QtGui import QImage, QPixmap, QMovie
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QStackedWidget, QFormLayout, QLabel, QLineEdit, QComboBox, QMessageBox, QDesktopWidget, QStatusBar
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import Qt, QTimer
 
 from datetime import date
 from cv2 import VideoCapture
 import mysql.connector as sql
 from os import getcwd
 
+class CONNECT:
+
+    def __init__(self):
+
+        self.DATAB, self.CURSOR = self.connect()
+
+    def connect(self):
+
+        DATAB = sql.connect(
+            user='root', password='SchooL1@', database='userData', 
+            host='127.0.0.1' if ROOT == 'C:' else '192.168.1.43'
+            )
+        CURSOR = DATAB.cursor(buffered=True)
+
+        return DATAB, CURSOR
+
+    def execute(self, statement, arguments=None, many=0, commit=0):
+        
+        for _ in range(20):
+            try:
+                if many: self.CURSOR.executemany(statement, arguments)
+                else: self.CURSOR.execute(statement, arguments)
+
+                if commit: self.DATAB.commit()
+                else: return self.CURSOR.fetchall()
+
+            except sql.errors.OperationalError: continue
+            
+            except sql.errors.ProgrammingError: return list()
+
+            except: self.DATAB, self.CURSOR = self.connect()
+
+    def close(self): self.DATAB.close()
+
 ROOT = getcwd()[:2].upper()
-DATAB = sql.connect(
-    user='root', password='SchooL1@', database='userData', 
-    host='192.168.1.43' if __file__.startswith(('e:\\', 'e:/')) else '127.0.0.1'
-    )
-CURSOR = DATAB.cursor(buffered=True)
+CONNECTION = CONNECT()
 BASE = f'SELECT REPLACE(path, "C:", "{ROOT}"), tags, artist, stars, rating, type FROM imageData'
 COMIC = 'SELECT path FROM comics'
 UPDATE = f'UPDATE imageData SET date_used="{date.today()}" WHERE path=REPLACE(%s, "{ROOT}", "C:")'
