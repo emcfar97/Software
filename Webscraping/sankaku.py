5798430f426fd27c1c18153675c27f0b2caacc28
Ethan McFarland <emc1140@hotmail.com>
2020-07-31 23:44:00 -0500

    Added tag grabber

--- Webscraping/sankaku.py
+++ Webscraping/sankaku.py
@@ -1,3 +1,7 @@
+from selenium.common.exceptions import WebDriverException
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 SITE = 'sankaku'
 MODE = {
     0:['idol', 0],
@@ -33,25 +37,27 @@ def initialize(mode, url='?tags=fav%3Achairekakia', query=0):
 
     DATAB.commit()
 
-def page_handler(hrefs, mode):
+def page_handler(driver, hrefs, mode):
 
     if not hrefs: return
     size = len(hrefs)
 
     for num, (href,) in enumerate(hrefs):
-
+        
         progress(size, num, SITE)
         page_source = requests.get(
             f'https://{mode[0]}.sankakucomplex.com{href}'
             ).content      
         html = bs4.BeautifulSoup(page_source, 'lxml')
-        if html.find(text=re.compile('(Too many requests)|(Please slow down)')):
+        while html.find(text=re.compile('(Too many requests)|(Please slow down)')):
             time.sleep(60)
             page_source = requests.get(
                 f'https://{mode[0]}.sankakucomplex.com{href}'
                 ).content   
             html = bs4.BeautifulSoup(page_source, 'lxml')
-
+        image = f'https:{html.find(id="highres", href=True).get("href")}'
+        name = get_name(image.split('/')[-1].split('?e=')[0], mode[1])
+            
         metadata = ' '.join(
             '_'.join(tag.text.split()[:-2]) for tag in 
             html.findAll(class_='tag-type-medium')
@@ -62,32 +68,26 @@ def page_handler(hrefs, mode):
             )
         artists = [
             '_'.join(artist.text.split()[:-2]) for artist in 
-            html.findAll(class_=re.compile('tag-type-(artist)|(idol)'))
+            html.findAll(class_=re.compile('tag-type-artist|idol|studio'))
             ]
+        if len(tags.split()) < 10:
+            save_image(name, image)
+            tags = tags + get_tags(driver, name)
         tags, rating, exif = generate_tags(
             tags, metadata, True, artists, True
-            )
-        
-        try:
-            image = f'https:{html.find(id="highres", href=True).get("href")}'
-            name = get_name(image.split('/')[-1].split('?e=')[0], mode[1])
-            save_image(name, image, exif)
-            hash_ = get_hash(name)
-        except: continue
+            )        
+        if not save_image(name, image, exif): continue
+        hash_ = get_hash(name)
 
         try:
             execute(UPDATE[3], (
-                name, f"{' '.join(artists)}", f' {tags} ', 
-                rating, image, hash_, mode[1], href),
+                name, f"{' '.join(artists)}", tags, 
+                rating, image, hash_, href),
                 commit=1
                 )
         except sql.errors.IntegrityError:
-            execute(UPDATE[3], (
-                f'202 - {href}', f"{' '.join(artists)}", 
-                f' {tags} ', rating, image, hash_, mode[1], href),
-                commit=1
-                )
-        except (sql.errors.OperationalError, sql.errors.DatabaseError): continue
+            execute('DELETE FROM imageData WHERE href=%s', (href,), commit=1)
+        except sql.errors.DatabaseError: continue
     
     progress(size, size, SITE)
 
@@ -96,14 +96,21 @@ def setup(initial=True, mode=1):
     mode = MODE[mode]
 
     try:
+        driver = get_driver(True)
         if initial: initialize(mode)
         CURSOR.execute(f'{SELECT[2]} AND type=%s', (SITE, mode[1]))
-        page_handler(CURSOR.fetchall(), mode)
-    except Exception as error: print(f'{SITE}: {error}')
+        page_handler(driver, CURSOR.fetchall()[2:], mode)
+    except WebDriverException:
+        user = input(f'\n{SITE}: Browser closed\nContinue? ')
+        if user.lower() in 'yes': setup(False)
+    except Exception as error: print(f'\n{SITE}: {error}')
+    
+    try: driver.close()
+    except: pass
 
 if __name__ == '__main__':
     
     from utils import *
-    setup(mode=0)
+    setup(mode=1)
 
 else: from .utils import *
