73b13dff439ce418115a71a4261963a58b4aae63
Ethan McFarland <emc1140@hotmail.com>
2020-07-31 23:27:09 -0500

    Tweaks & fixes, converted gallery list to generator

--- GUI/previewView.py
+++ GUI/previewView.py
@@ -3,10 +3,9 @@ from . import *
 
 class Preview(QWidget):
     
-    def __init__(self, parent, type_):
+    def __init__(self, parent):
         
         super().__init__(parent)
-        self.type = type_
         self.configure_gui()
         self.create_widgets()
     
@@ -14,17 +13,16 @@ class Preview(QWidget):
             
         size = self.parent().size() 
         
-        if self.type == 'Manage Data':
+        if self.parent().windowTitle() == 'Manage Data':
             self.setGeometry(
                 int(size.width() // 2), 0, 
                 int(size.width() // 2), size.height() - 15
                 )
             self.setStyleSheet('background: white')
             
-        elif self.type == 'Gesture Draw':
+        elif self.parent().windowTitle() == 'Gesture Draw':
             self.setGeometry(
-                0, 0, 
-                size.width(), size.height() - 20
+                0, 0, size.width(), size.height()
                 ) 
             self.setStyleSheet('background: black')
     
@@ -37,43 +35,35 @@ class Preview(QWidget):
         
     def start(self, gallery, time):
     
-        self.index = 0
         self.gallery = gallery
-        self.time = time
-        self.move(0)
+        self.path = next(self.gallery)
+        self.show_image(self.path)
         
-        self.timer.time = self.time
+        self.time = self.timer.time = time
         self.timer.setGeometry(
             self.width() * .8, self.height() * .8, 
             75, 75
-            )        
-        min, sec = divmod(time, 60)
-        self.timer.setText(f'{min}:{sec:02d}')
+            )
+        self.timer.setStyleSheet('background: white; font: 20px')
+        self.timer.setText('{}:{:02}'.format(*divmod(time, 60)))
         self.timer.timer.start(1000)                
                 
-    def move(self, delta):
-        
-        self.index = (self.index + delta) % len(self.gallery)
-        self.show_image(self.gallery[self.index])
-    
     def show_image(self, path):
-                    
-        try:
-            width, height = self.width(), self.height()
-                
+
+        if path: 
             if path.endswith(('.mp4', '.webm')):
                 
                 image = VideoCapture(path).read()[-1]
                 path = qimage2ndarray.array2qimage(image).rgbSwapped()
             
             pixmap = QPixmap(path).scaled(
-                width, height, Qt.KeepAspectRatio, 
+                self.width(), self.height(), Qt.KeepAspectRatio, 
                 transformMode=Qt.SmoothTransformation
                 )
             # ratio = pixmap.width() / pixmap.height()
-            # self. = not (.5 <= ratio <= 2)
+            # scrollbar = not (.5 <= ratio <= 2)
 
-        except (ValueError, AttributeError): pixmap = QPixmap()
+        else: pixmap = QPixmap()
                 
         self.label.setPixmap(pixmap)
     
@@ -91,42 +81,38 @@ class Countdown(QLabel):
         self.timer = QTimer()
         self.setAlignment(Qt.AlignCenter)
         self.timer.timeout.connect(self.countdown)
-        self.setStyleSheet('background: white; font: 20px')
 
     def countdown(self):
         
-        while self.time:
+        if self.time:
             
             self.time -= 1
-            min, sec = divmod(self.time, 60)
-            self.setText(f'{min}:{sec:02d}')
+            self.setText('{}:{:02}'.format(*divmod(self.time, 60)))
             self.setStyleSheet(
                 f'''background: white; 
                 color: {"red" if self.time <= 5 else "black"}; 
                 font: 20px'''
                 )   
         
-        else: 
+        else:
             parent = self.parent()
-            CURSOR.execute(UPDATE, (parent.gallery[parent.index],))
-            DATAB.commit()
+            CONNECTION.execute(UPDATE, (parent.path,), commit=1)
             
-            if parent.index + 1 < len(parent.gallery):
-                
-                parent.move(+1)
+            try:
+                parent.path = next(parent.gallery)
+                parent.show_image(parent.path)
                 self.time = parent.time
-                min, sec = divmod(self.time, 60)
-                self.setText(f'{min}:{sec:02d}')
+                self.setText('{}:{:02}'.format(*divmod(self.time, 60)))
                 self.setStyleSheet(
                     f'''background: white; 
                     color: {"red" if self.time <= 5 else "black"}; 
                     font: 20px'''
                     )  
-                self.timer.start(1000)  
-                
-            else:
+
+            except StopIteration:
+
                 self.timer.stop()
-                parent.label.setPixmap(QPixmap())
+                parent.show_image(None)
                 self.setText('End of session')
                 self.setStyleSheet(
                     'background: black; color: white; font: 17px'
