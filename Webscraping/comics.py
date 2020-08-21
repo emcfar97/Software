60ac3d8a5bf59265253eb47e337bd7a040c1e693
--1stRayos <--emc1140@hotmail.com>
2020-08-21 12:48:19 -0500

    Minor tweaks & fixes

--- Webscraping/comics.py
+++ Webscraping/comics.py
@@ -1,8 +1,8 @@
 import shutil, re
-from . import ROOT, CONNECTION, WEBDRIVER
+from . import ROOT, CONNECTION, WEBDRIVER, INSERT
 from .utils import get_name, get_hash, get_tags, generate_tags
 
-path = ROOT / r'Downloads\Images\Comics'
+path = ROOT.parent / r'Downloads\Images\Comics'
 driver = WEBDRIVER()
 
 def start():
@@ -12,7 +12,7 @@ def start():
         targets = re.findall('\[.+\]|\(\w+\)', folder.stem.lower())
         artist = ' '.join('_'.join(i[1:-1].split()) for i in targets)
         images = [
-            (shutil.copy(file, get_name(file, 2, 1)), num)
+            (num, shutil.copy(file, get_name(file, 2, 1)))
             for num, file in enumerate(folder.iterdir())
             ]
         cover = images[0][1]
@@ -20,9 +20,8 @@ def start():
         for num, image in images:
 
             hash_ = get_hash(image)
-            tags = get_tags(driver, image, comic=1)
             tags, rating = generate_tags(
-                general=tags, custom=True, rating=True, exif=False
+                general=get_tags(driver, image), custom=True, rating=True, exif=False
                 )
             CONNECTION.execute(
                 INSERT[4], 
