0302de0b9f279e73c8207578b67a599fb38d3d8a
--1stRayos <--emc1140@hotmail.com>
2020-08-21 12:47:40 -0500

    Converted os to pathlib

--- backupFiles.py
+++ backupFiles.py
@@ -1,56 +1,64 @@
-import os, re
+import re
+from os import walk
+from pathlib import Path
 from datetime import date
 from shutil import copy, copytree
-from win32api import GetLogicalDriveStrings
-from os.path import join, getmtime, isfile, dirname, splitext
+from win32con import DRIVE_REMOVABLE
+from win32.win32file import GetDriveType
+from win32.win32api import GetLogicalDriveStrings
+
+SOURCE = Path.home() / 'Dropbox'
+
+def get_drives(drive_types=(DRIVE_REMOVABLE,)):
+
+    drives = GetLogicalDriveStrings().split("\x00")
+    drives = [item for item in drives if item]
+    
+    return [
+        item[:2] for item in drives 
+        if drive_types is None or 
+        GetDriveType(item) in drive_types
+        ]
 
 def last_modified(path):
 
-    try: modified = date.fromtimestamp(getmtime(path))
+    try: modified = path.stat().st_mtime
     except: return
-    difference = date.today() - modified
+    difference = date.today() - date.fromtimestamp(modified)
 
     return difference.days <= 15
 
 def get_version(paths):
 
-    latest = max(
-        paths, key=lambda path: 
-        int(re.search(' [0-9]+', path).group()[1:])
-        )
-    version = int(re.search(' [0-9]+', latest).group()[1:])
+    latest = str(sorted(paths)[-1])
+    version = int(re.findall(' \d+', latest)[0])
 
-    return re.sub(' [0-9]+', f' {version + 1}', latest)
+    return re.sub(' \d+', f' {version + 1:02}', latest)
 
-ROOT = os.getcwd()[:3].upper()
-SOURCE = rf'{ROOT}Users\Emc11\Dropbox'
-drives = GetLogicalDriveStrings().split('\000')[:-1]
-drives.remove(ROOT)
-
-for drive in drives:
+for drive in get_drives():
+# for drive in ['E:\\', 'D:\\']:
     
     print(drive)
 
-    for root, dirs, files in os.walk(drive):
+    for root, dirs, files in walk(drive):
 
         targets = {}
+        root = Path(root)
+        head = SOURCE / Path(*root.parts[1:])
         
-        for path in dirs + files:
+        for path in root.iterdir():
             
-            dropbox = re.sub(
-                r' - \d+', '', join(root.replace(drive[:-1], SOURCE), path)
-                )
-            if dropbox.endswith('scriv'):
+            dropbox = head / re.sub(r' - \d+', '', path.name)
+            if dropbox.suffix == '.scriv':
                 
-                head, ext = splitext(dropbox)
-                dropbox = dirname(head) + ext
+                dropbox = dropbox.parent.with_suffix(dropbox.suffix)
 
-            if re.search(r'\D - \d', path) and last_modified(dropbox):
+            if re.search(r'\D - \d', path.stem) and last_modified(dropbox):
                 
-                targets[dropbox] = targets.get(dropbox, []) + [join(root, path)]
+                targets[dropbox] = targets.get(dropbox, []) + [path]
 
         for key, val in targets.items():
-
+            
             val = get_version(val)
 
             if isfile(key): copy(key, val)
