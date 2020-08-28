a9c0e958332b8b27cb24c6bdfdecf9de59f5e8b5
--1stRayos <--emc1140@hotmail.com>
2020-08-27 21:05:11 -0500

    Add progress bar

--- Webscraping/comics.py
+++ Webscraping/comics.py
@@ -1,13 +1,18 @@
 import shutil, re
 from . import ROOT, CONNECTION, WEBDRIVER, INSERT
-from .utils import get_name, get_hash, get_tags, generate_tags
+from .utils import progress, get_name, get_hash, get_tags, generate_tags
 
 path = ROOT.parent / r'Downloads\Images\Comics'
-driver = WEBDRIVER()
+driver = WEBDRIVER(True)
 
 def start():
 
-    for folder in path.iterdir():
+    folders = path.iterdir()
+    size = len(folders)
+
+    for num, folder in enumerate(folders):
+
+        progress(size, num, '\nComics')
 
         targets = re.findall('\[.+\]|\(\w+\)', folder.stem.lower())
         artist = ' '.join('_'.join(i[1:-1].split()) for i in targets)
@@ -17,7 +22,10 @@ def start():
             ]
         cover = images[0][1]
 
+        size_ = len(images)
         for num, image in images:
+
+            progress(size_, num, artist)
 
             hash_ = get_hash(image)
             tags, rating = generate_tags(
@@ -25,9 +33,9 @@ def start():
                 )
             CONNECTION.execute(
                 INSERT[4], 
-                (str(image), artist, tags, rating, 2, hash_, cover.stem, num), 
-                commit=1
+                (str(image), artist, tags, rating, 2, hash_, cover.stem, num)
                 )
+        CONNECTION.commit()
         shutil.rmtree(folder)
 
     driver.close()
