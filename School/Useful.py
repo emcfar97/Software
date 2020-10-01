88041a9edf1eac16af6e45f44949460c7c2ccdff
--1stRayos <--emc1140@hotmail.com>
2020-10-01 17:07:31 -0500

    Changed print to pprint

--- School/Useful.py
+++ School/Useful.py
@@ -4,34 +4,34 @@ def addition(start, end, num):
 
     global matrix
 
-    print(f'\nAdd {num}R{sub[start]} to R{sub[end]}')
+    print(f'\nR{sub[end]}{num:+}R{sub[start]}')
 
     matrix = matrix.elementary_row_op(
         'n->n+km', row1=start-1, row=end-1, k=num
         )
-    print(matrix)
+    sympy.pprint(matrix)
 
 def multiply(start, num):
 
     global matrix
 
-    print(f'\nMultiply {num}R{sub[start]}')
+    print(f'\n{num}R{sub[start]}')
     
     matrix = matrix.elementary_row_op(
         'n->kn', row=start-1, k=num
         )
-    print(matrix)
+    sympy.pprint(matrix)
 
 def exchange(start, end):
 
     global matrix
 
-    print(f'\nExchange R{sub[start]} with R{sub[end]}')
+    print(f'\nR{sub[start]} <-> R{sub[end]}')
     
     matrix = matrix.elementary_row_op(
         'n<->m', row1=start-1, row2=end-1
         )
-    print(matrix)
+    sympy.pprint(matrix)
 
 def transpose():
 
@@ -40,12 +40,16 @@ def transpose():
     print('\nTranspose matrix')
 
     matrix = matrix.transpose()
-    print(matrix)
+    sympy.pprint(matrix)
 
 sub = [chr(i) for i in range(8320, 8330)]
 
 matrix = sympy.Matrix([
+    [1,2],
+    [3,4],
+    [5,6]
     ])
 
-print(matrix)
+sympy.pprint(matrix)
+addition(1,2,-3)
 print(matrix.rref())
