18bcdb1ffda2581fd01cf5e4192e7f20073f4a4a
--1stRayos <--emc1140@hotmail.com>
2021-02-10 15:20:59 -0600

    Tweaks & fixes

--- Webscraping/Favorites/foundry.py
+++ Webscraping/Favorites/foundry.py
@@ -1,31 +1,31 @@
-from urllib.parse import urlparse
 from .. import CONNECT, INSERT, SELECT, UPDATE, WEBDRIVER
-from ..utils import PATH, Progress, get_hash, bs4, re
+from ..utils import PATH, Progress, bs4, re
 
 SITE = 'foundry'
 
-def initialize(url='/user/Chairekakia/faves/pictures/enterAgree/1/size/1550/page/1', query=0):
+def initialize(url, query=0):
     
     def next_page(pages):
         
-        try: return pages.contents[0].get('href')
-        except IndexError: return False
+        if 'hidden' in pages.get('class'): return
+        else: return pages.contents[0].get('href')
 
     DRIVER.get(f'http://www.hentai-foundry.com{url}')
     if not query:
-        DRIVER.find('//*[@id="frontPage"]', click=True, type_=1)
         query = set(CONNECTION.execute(SELECT[1], (SITE,), fetch=1))
+        if element := DRIVER.find('//*[@id="frontPage"]', type_=1):
+            element.click()
     html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
     hrefs = [
-        (*href, SITE) for href in {(target.get('href'),) for target in 
+        (*href, SITE) for href in 
+        {(target.get('href'),) for target in 
         html.findAll(class_='thumbLink')} - query
         ]
     CONNECTION.execute(INSERT[1], hrefs, many=1)
 
-    next = next_page(html.find('li', class_='next')) 
+    next = next_page(html.find(class_='next')) 
     if hrefs and next: initialize(next, query)
-
-    CONNECTION.commit()
+    else: CONNECTION.commit()
 
 def page_handler(hrefs):
 
@@ -34,22 +34,23 @@ def page_handler(hrefs):
 
     for href, in hrefs:
         
-        print(progress)
-
         DRIVER.get(f'http://www.hentai-foundry.com{href}')
+        if element := DRIVER.find('//*[@id="frontPage"]', type_=1):
+            element.click()
         DRIVER.find(
             '//body/main/div/section[1]/div[2]/img', click=True, type_=1
             )
-        html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
+        try: html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
+        except: continue
 
         artist = html.find(class_='breadcrumbs').text.split(' » ')[1]
         image = f'http:{html.find(class_="center", src=True).get("src")}'
-        name = re.sub(f'{artist}-\d+', f'{artist}', image.split('/')[-1])
+        name = re.sub(f'({artist})-\d+', r'\1 - ', image.split('/')[-1])
         name = PATH / 'Images' / SITE / name
 
         CONNECTION.execute(UPDATE[2], (str(name), image, href), commit=1)
     
-    print(progress)
+        print(progress)
 
 def start(initial=True, headless=True):
     
@@ -57,6 +58,8 @@ def start(initial=True, headless=True):
     CONNECTION = CONNECT()
     DRIVER = WEBDRIVER(headless)
     
-    if initial: initialize()
+    if initial:
+        url = DRIVER.login(SITE)
+        initialize(url)
     page_handler(CONNECTION.execute(SELECT[3], (SITE,), fetch=1))
     DRIVER.close()
