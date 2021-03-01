f6ed419f65be7fde6c594f8532ed4ba9d519d4ef
--1stRayos <--emc1140@hotmail.com>
2021-03-01 14:34:25 -0600

    Converted to standard format for module

--- Webscraping/Photos/elitebabes.py
+++ Webscraping/Photos/elitebabes.py
@@ -1,62 +1,71 @@
-from .. import CONNECT, WEBDRIVER, INSERT
-from ..utils import Progress, save_image, get_hash, get_name, get_tags, generate_tags, bs4, re, requests
+from .. import CONNECT, WEBDRIVER, INSERT, SELECT, DELETE
+from ..utils import IncrementalBar, save_image, get_hash, get_name, get_tags, generate_tags, bs4, re, requests
 
 SITE = 'elitebabes'
 
-def next_page(page):
-             
-    try: return page.contents[0].get('href').split('/')[-2]
-    except IndexError: return False
+def initialize(url=1, query=0):
 
-def page_handler(hrefs, artist):
+    def next_page(page):
+                
+        try: return page.contents[0].get('href').split('/')[-2]
+        except IndexError: return False
+
+    if not query:
+        query = set(CONNECTION.execute(SELECT[0], (SITE,), fetch=1))
+
+    DRIVER.get(f'https://www.{SITE}.com/my-favorite-galleries/page/{url}/')
+    html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
+    hrefs = [
+        (target.get('href').split('/')[-2][:-6], SITE) for target in 
+        html.find(class_='gallery-a e').findAll(href=True)
+        if (target.get('href').split('/')[-2][:-6],) not in query
+        ]
+
+    next = next_page(html.find(class_='next'))
+    if hrefs and next: return hrefs + initialize(next, query)
+    else: return hrefs
+
+def page_handler(hrefs):
     
     if not hrefs: return
-    progress = Progress(len(hrefs), SITE)
+    progress = IncrementalBar(SITE, max=len(hrefs))
 
-    for href in hrefs:
+    for href, in hrefs:
         
-        src = href.get('href')
-        if (name:=get_name(src, 0, 1)).exists(): continue
-        if not save_image(name, src): continue
+        page_source = requests.get(f'https://www.{SITE}.com/{href}')
+        html = bs4.BeautifulSoup(page_source.content, 'lxml')
+        artist = html.find(href=re.compile(
+            f'https://www.{SITE}.com/model/.+'
+            )).get('href')
+        artist = artist.split('/')[-2].replace('-', '_')
 
-        tags, rating, exif = generate_tags(
-            general=get_tags(DRIVER, name, True), 
-            custom=True, rating=True, exif=True
-            )
-        save_image(name, src, exif)
-        artist = artist.replace('-', '_')
-        hash_ = get_hash(name)
+        for image in html.find(class_='list-justified-container').findAll('a'):
 
-        CONNECTION.execute(INSERT[3], 
-            (name.name, artist, tags, rating, 1, hash_, src, SITE), 
-            commit=1
-            )
+            src = image.get('href')
+            name = get_name(src, 0, 1)
+            if not save_image(name, src): break
+
+            tags, rating, exif = generate_tags(
+                general=get_tags(DRIVER, name, True), 
+                custom=True, rating=True, exif=True
+                )
+            save_image(name, src, exif)
+            hash_ = get_hash(name)
+
+            CONNECTION.execute(INSERT[3],
+                (name.name, artist, tags, rating, 1, hash_, src, SITE, href), 
+                )
+        else: CONNECTION.execute(DELETE[0], (href,), commit=1)
     
-        print(progress)
+        progress.next()
 
-def start(page=1, headless=True):
+def start(initial=True, headless=True):
         
     global CONNECTION, DRIVER
     CONNECTION = CONNECT()
     DRIVER = WEBDRIVER(headless)
 
-    # DRIVER.login(SITE)
-    while page:
-
-        DRIVER.get(f'https://www.{SITE}.com/my-favorite-galleries/page/{page}/')
-        html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
-        
-        for target in html.find(class_='gallery-a e').findAll(href=True):
-            
-            page_source = requests.get(target.get('href'))
-            page = bs4.BeautifulSoup(page_source.content, 'lxml')
-            images = page.find(class_='list-justified-container')
-            artist = page.find(href=re.compile(
-                f'https://www.{SITE}.com/model/.+'
-                )).get('href')
-
-            page_handler(images.findAll('a'), artist.split('/')[-2])
-
-        page = next_page(html.find(class_='next'))
-
+    if initial: 
+        CONNECTION.execute(INSERT[0], initialize(), many=1, commit=1)
+    page_handler(CONNECTION.execute(SELECT[2], (SITE,), fetch=1))
     DRIVER.close()
