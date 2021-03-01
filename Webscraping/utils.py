620a77bd8c930278eeb8a9b53aac613e0d9655a1
--1stRayos <--emc1140@hotmail.com>
2021-03-01 14:39:55 -0600

    Tweaks/fixes

--- Webscraping/utils.py
+++ Webscraping/utils.py
@@ -1,9 +1,11 @@
-import imagehash, piexif, bs4, requests, re, tempfile, hashlib, sys, ast
+import piexif, bs4, requests, re, tempfile, hashlib, ast
 from . import ROOT
 from os import path
 from math import log
 from io import BytesIO
 from ffprobe import FFProbe
+from imagehash import dhash
+from progress.bar import IncrementalBar
 from PIL import Image, UnidentifiedImageError
 from cv2 import VideoCapture, imencode, cvtColor, COLOR_BGR2RGB
 
@@ -38,17 +40,19 @@ GENERAL = {
     'japanese_clothes': 'yamakasa|tabi|sarashi|fundoshi|hakama|short_yukata|yukata|short_kimono|kimono|geta|happi|zori',
     'male_focus': '(male_focus OR (solo AND 1boy) OR (1boy AND NOT (1girl OR 2girls OR 3girls OR 4girls OR multiple_girls)))',
     'muscular': '(solo OR (1girl AND NOT (1boy OR 2boys OR 3boys OR 4boys OR multiple_boys))) AND (muscular OR muscle OR muscular_female OR abs)', 
+    'nopan': 'no_panties',
     'nude': 'nude AND NOT functionally_nude',
     'functionally_nude': '(functionally_nude OR (bottomless AND topless)) AND NOT nude', 
     'open_clothes': 'open_clothes|open_coat|open_jacket|open_shirt|open_robe|open_kimono|open_fly|open_shorts', 
     'orgasm': 'orgasm|ejaculation', 
-    'piercings': 'piercings|earrings|navel_piercings|areola_piercing|back_piercing|navel_piercing|nipple_piercing|ear_piercing|eyebrow_piercing|eyelid_piercing|lip_piercing|nose_piercing|tongue_piercing|clitoris_piercing|labia_piercing|penis_piercing|testicle_piercing|nipple_chain', 
+    'oshiri': 'ass'
+    'piercing': 'piercings|earrings|navel_piercings|areola_piercing|back_piercing|navel_piercing|nipple_piercing|ear_piercing|eyebrow_piercing|eyelid_piercing|lip_piercing|nose_piercing|tongue_piercing|clitoris_piercing|labia_piercing|penis_piercing|testicle_piercing|nipple_chain', 
     'presenting': 'presenting OR top-down_bottom-up OR ((spread_legs OR spread_pussy) AND (trembling OR (heavy_breathing OR breath) OR (parted_lips AND NOT clenched_teeth))) OR spread_pussy', 
     'pussy': 'pussy|vagina', 
     'pussy_juice': 'pussy_juice|pussy_juices|pussy_juice_trail|pussy_juice_puddle|pussy_juice_stain|pussy_juice_drip_through_clothes', 
     'revealing_clothes': 'revealing_clothes|torn_clothes|micro_bikini|crop_top|pussy_peek|midriff|cleavage_cutout|wardrobe_malfunction|breast_slip|nipple_slip|areola_slip|no_panties|no_bra|pelvic_curtain|side_slit|breasts_outside|see-through|partially_visible_vulva|functionally_nude|breastless_clothes|bare_shoulders|one_breast_out',
-    'sex': 'sex|aftersex|vaginal|anal|facial|oral|fellatio|cunnilingus|handjob|frottage|tribadism|group_sex|hetero|yaoi|yuri', 
-    'sex_toys': 'sex_toys|vibrator|dildo|butt_plug|artificial_vagina',
+    'sex': '(sex AND aftersex AND vaginal AND anal AND facial AND oral AND fellatio AND cunnilingus AND handjob AND frottage AND tribadism AND group_sex AND hetero AND yaoi AND yuri) AND NOT solo', 
+    'sex_toy': 'sex_toys|vibrator|dildo|butt_plug|artificial_vagina',
     'solo': 'solo OR (1girl AND NOT (1boy OR 2boys OR 3boys OR 4boys OR multiple_boys)) OR (1boy AND NOT (1girl OR 2girls OR 3girls OR 4girls OR multiple_girls) OR NOT sex)', 
     'standing_sex': '(standing_on_one_leg OR (standing AND (leg_up OR leg_lift))) AND sex',
     'suggestive': 'sexually_suggestive OR (naughty_smile OR fellatio_gesture OR teasing OR blush OR spread_legs OR pulled_by_self OR lifted_by_self OR (come_hither OR beckoning) OR (tongue_out AND (open_mouth OR licking_lips)) OR (bent_over AND (looking_back OR looking_at_viewer)) OR (trembling OR (saliva OR sweat) OR ((heavy_breathing OR breath) OR (parted_lips AND NOT clenched_teeth))) OR (skirt_lift OR bra_lift OR dress_lift OR shirt_lift OR wind_lift OR breast_lift OR kimono_pull) AND NOT (vaginal OR anal OR sex OR erection OR aftersex OR ejaculation OR pussy OR penis))', 
@@ -61,7 +65,6 @@ CUSTOM = {
     'intercrural': 'thigh_sex',
     'loops': 'loops|thigh_strap|necklace|neck_ring|anklet|bracelet|armlet',  
     'naked_clothes': 'naked_belt|naked_apron|naked_shirt|naked_cape|naked_overalls|naked_ribbon|naked_cloak|naked_bandage|naked_robe', 
-    'sexy': 'NOT (nude OR topless OR (bottomless OR no_panties))',
     'slender': '(solo OR (1girl AND NOT (1boy OR 2boys OR 3boys OR 4boys OR multiple_boys))) AND (slender OR NOT (((muscular OR muscle OR muscular_female OR toned OR abs) OR ((large_breasts OR huge_breasts OR gigantic_breasts) OR (plump OR fat) OR thick_thighs OR wide_hips OR huge_ass))))',
     'vagina': 'pussy',
     'underwear': 'underwear|panties|bra|briefs'
@@ -72,28 +75,6 @@ RATING = {
     1: 'NOT (sex OR aftersex OR hetero OR vaginal OR anal OR anus OR cum OR penis OR vagina OR pussy OR pussy_juice OR vaginal_juices OR spread_pussy OR erection OR clitoris OR anus OR oral OR fellatio OR fingering OR handjob OR masturbation OR object_insertion OR nipples OR areola OR areolae OR covered_nipples OR cameltoe OR wedgie OR torn_clothes OR pubic_hair OR topless OR bottomless OR sexually_suggestive OR nude OR wet_panties OR no_panties OR spanking OR bondage OR vore OR bdsm OR open_clothes OR revealing_clothes OR breast_slip OR areoala_slip OR spread_ass OR orgasm OR vibrator OR sex_toy OR bulge OR lactation OR panty_pull OR panties_around_leg OR panties_removed OR partially_visible_vulva OR breast_sucking OR birth OR naked_clothes OR used_condom OR (suggestive AND (blush AND (spread_legs OR undressing OR erect_nipples OR ((miniskirt OR microskirt) AND underwear) OR (clothes_lift AND underwear)))))'
     }
     
-class Progress:
-    
-    def __init__(self, size, site, length=20):
-        
-        self.size = size
-        self.site = site
-        self.length = length
-        self.left = (i for i in range(self.size))
-        print()
-        
-    def __str__(self):
-        
-        try: left = next(self.left)
-        except: left = self.size
-        percent =  left / self.size
-        ratio = f'({left}/{self.size})'
-        padding = int(percent * self.length)
-        bar = f'[{"|" * padding}{" " * (self.length-padding)}]'
-        sys.stdout.write("\033[F")
-        
-        return f'{self.site}  —  {bar} {percent:>4.0%} {ratio}'
-
 def save_image(name, image=None, exif=b''):
     '''Save image to name (with optional exif metadata)'''
 
@@ -162,14 +143,13 @@ def get_hash(image, src=False):
     image.thumbnail([32, 32])
     image = image.convert('L')
 
-    return f'{imagehash.dhash(image)}'
+    return f'{dhash(image)}'
 
 def get_tags(driver, path, filter=False):
 
     tags = set()
     frames = []
-    try: video = path.suffix in ('.gif', '.webm', '.mp4')
-    except: video = False
+    video = path.suffix in ('.gif', '.webm', '.mp4')
 
     if video:
 
@@ -220,15 +200,15 @@ def get_tags(driver, path, filter=False):
     else:
         if video: temp_dir.cleanup()
         if filter:
-            tags.discard('3d')
-            tags.discard('cum')
-            tags.discard('sex')
-            tags.discard('censored')
-            tags.discard('doggystyle')
-            tags.discard('missionary')
-            tags.discard('cowgirl_position')
-            tags.discard('mosaic_censoring')
-            tags.discard('male_focus')
+            tags.difference_update({
+                '3d', 'censored',
+                'cum', 'cum_in_pussy',
+                'sex', 'sex_from_behind', 'vaginal', 'anal',
+                'doggystyle', 'missionary', 'cowgirl_position',
+                'mosaic_censoring',
+                'male_focus', 'no_humans', 'multiple_boys', 'multiple_girls', 
+                'photorealistic', 'realistic'
+                })
     
     return ' '.join(tags)
 
@@ -283,8 +263,8 @@ def generate_tags(general, metadata=0, custom=0, artists=[], rating=0, exif=1):
         rating.append(exif)
     
     tags = re.sub(
-        '(photorealistic|photo|realistic|(rating|score):\w+|solo_focus|uncensored|vagina|hetero) ', '', ' '.join(tags)
-        )
+        ' ((rating|score):\w+|nopan|oshiri|solo_focus|uncensored|vagina|virgin|hetero|yuri|yaoi) ', ' ', f' {" ".join(tags)} '
+        ).strip()
         
     return tags if rating is None else [tags] + rating
 
