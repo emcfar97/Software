02bf945134653fae3f1fc2fdd4e07256aa7df9ba
Rayos1 <emc1140@hotmail.com>
2021-03-31 19:27:11 -0500

    General update

--- test.py
+++ test.py
@@ -1,123 +1,51 @@
 def Controller(arg=None):
     
-    import Webscraping
-    from Webscraping import insert_records
-    from Webscraping.Photos import elitebabes, posespace, blogspot
+    if arg == 0:
 
-    elitebabes.start()
-    # posespace.start(0)
-    # blogspot.start(1, 0)
-    
-    if arg == 0: Webscraping.start()
+        import Webscraping
+
+        Webscraping.start()
+        Get_Starred()
+
     elif arg == 1:
+
+        from Webscraping import insert_records
+
         insert_records.start()
+        Remove_redundancies()
         Update_Autocomplete()
+    
+    else:
 
-def Artist_statistics():
+        from Webscraping.Photos import posespace, blogspot
 
-    from Webscraping import CONNECT
-
-    CONNECTION = CONNECT()
-
-    SELECT = 'SELECT artist FROM imagedata GROUP BY artist HAVING COUNT(artist) > 100 ORDER BY artist'
-    STATS = '''SELECT (
-        SELECT COUNT(*) FROM imagedata 
-        WHERE MATCH(tags, artist) AGAINST(%s IN BOOLEAN MODE) AND stars
-        ) AS TOTAL,
-        (
-        SELECT SUM(stars) FROM imagedata 
-        WHERE MATCH(tags, artist) AGAINST(%s IN BOOLEAN MODE)
-        ) AS STARS
-        '''
-
-    for artist, in CONNECTION.execute(SELECT, fetch=1)[1:101]:
-
-        sum, star = CONNECTION.execute(STATS, (artist, artist), fetch=1)
-        try: print(f'{artist.strip():<25} (Num: {sum:>4}, Stars: {star:>4}): {star / (sum*5):>4.2%}')
-        except: continue
+        # posespace.start(0)
+        # blogspot.start(1, 0)
 
 def Remove_redundancies():
 
     from Webscraping import CONNECT
 
-    CONNECTION = CONNECT()        
-    SELECT = 'SELECT path, artist, tags FROM imageData WHERE path Like "C%"'
+    MYSQL = CONNECT()        
+    SELECT = 'SELECT path, artist, tags FROM imageData WHERE NOT ISNULL(path)'
     UPDATE = 'UPDATE imageData SET artist=%s, tags=%s WHERE path=%s'
 
-    for (path, artist, tags,) in CONNECTION.execute(SELECT, fetch=1):
+    for (path, artist, tags,) in MYSQL.execute(SELECT, fetch=1):
 
         artist = f' {" ".join(set(artist.split()))} '
         tags = f' {" ".join(set(tags.split()))} '
-        CONNECTION.execute(UPDATE, (artist, tags, path))
+        MYSQL.execute(UPDATE, (artist, tags, path))
 
-    CONNECTION.commit()
-
-def Normalize_database():
-
-    from os import path
-    from pathlib import Path
-    from Webscraping import CONNECT, ROOT
-    
-    CONNECTION = CONNECT()
-    DROPBOX = ROOT / path.expandvars(r'\Users\$USERNAME\Dropbox\ん')
-    CASE = r'''
-        case type
-        when 1 then CONCAT('{0}\エラティカ ニ\', path)
-        when 2 then CONCAT('{0}\エラティカ 三\', path)
-        when 3 then CONCAT('{0}\エラティカ 四\', path)
-        end
-        '''.format(DROPBOX).replace('\\', '\\\\')
-    SELECT = f'SELECT {CASE} FROM imageData WHERE NOT ISNULL(path)'
-    DELETE = 'DELETE FROM imageData WHERE path=SUBSTRING(%s, 34)'
-
-    database = set(
-        Path(path) for path, in CONNECTION.execute(SELECT, fetch=1)
-        )
-    windows = set(
-        DROPBOX.glob('エラティカ */*')
-        )
-    y = windows - database
-    x = database - windows
-    
-    for num, file in enumerate(y, 1):
-        file.replace(USER / r'\Downloads\Test' / file.name)
-    else:
-        try: print(f'{num} files moved')
-        except: print('0 files moved')
-    
-    for num, file in enumerate(x, 1):
-        CONNECTION.execute(DELETE, (str(file),), commit=1)
-    else:
-        try: print(f'{num} records deleted')
-        except: print('0 records deleted')
-
-def Check_Predictions():
-    
-    import cv2, random
-    from PIL import Image
-    from MachineLearning import Model, USER, np
-
-    model = Model('medium-01.hdf5')
-    path = USER / r'\Dropbox\ん'
-    glob = list(path.glob('エラティカ *\*jpg'))
-
-    for image in random.choices(glob, k=25):
-
-        prediction = model.predict(image)
-
-        image_ = np.aray(Image.open(image))
-        image_ = cv2.cvtColor(image_, cv2.COLOR_RGB2BGR)
-        cv2.imshow(prediction, image_)
-        cv2.waitKey(0)
+    MYSQL.commit()
 
 def Update_Autocomplete():
 
     from pathlib import Path
     from Webscraping import CONNECT
 
-    CONNECTION = CONNECT()
-    CONNECTION.execute('SET GLOBAL group_concat_max_len=10000000')
-    artist, tags = CONNECTION.execute(
+    MYSQL = CONNECT()
+    MYSQL.execute('SET GLOBAL group_concat_max_len=10000000')
+    artist, tags = MYSQL.execute(
         '''SELECT 
         GROUP_CONCAT(DISTINCT artist ORDER BY artist SEPARATOR ""), 
         GROUP_CONCAT(DISTINCT tags ORDER BY tags SEPARATOR "") 
@@ -130,56 +58,156 @@ def Update_Autocomplete():
     text = ('\n'.join(text)).encode('ascii', 'ignore')
     Path(r'GUI\autocomplete.txt').write_text(text.decode())
 
-def iterate():
+def Get_Starred():
 
-    import requests, bs4
-    from Webscraping import CONNECT
-    from Webscraping.utils import METADATA, IncrementalBar
+    from Webscraping import WEBDRIVER, CONNECT
+    import bs4
 
-    def fetch_row():
+    MYSQL = CONNECT()
+    DRIVER = WEBDRIVER()
+    UPDATE = 'UPDATE imageData SET stars=4 WHERE path=%s AND stars=0'
+    
+    show = '//body/div[1]/div[6]/div/div/div[1]/div/div/main/div/section[3]/div/div[2]/button'
+    address = '//body/div[1]/div[6]/div/div/div[1]/div/div/main/div/section[3]/div[2]/div/div[1]/div[1]/div[1]/button'
 
-        while True:
-            row = CONNECTION.CURSOR.fetchone()
-            if not row: break
-            yield row
+    DRIVER.get('https://www.dropbox.com/h', wait=4)
+    if (element:=DRIVER.find(show, fetch=1)).text == 'Show':
+        element.click()
+    html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
 
-    METADATA = set(METADATA.keys())
-    CONNECTION = CONNECT()
-    SELECT = 'SELECT {} FROM imageData WHERE site IN ("sankaku", "gelbooru") AND type=2'
-    UPDATE = 'UPDATE imageData SET tags=CONCAT(tags, %s) WHERE path=%s'
-    progress = IncrementalBar(
-        '', max=CONNECTION.execute(SELECT.format('COUNT(*)',), fetch=1)[0]
+    while starred:=html.findAll(class_='starred-item__content'):
+
+        paths = [(target.text,) for target in starred]
+        MYSQL.execute(UPDATE, paths, many=1, commit=1)
+        [DRIVER.find(address, click=True) for _ in range(5)]
+        html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
+
+    DRIVER.close()
+
+def Normalize_database():
+
+    from pathlib import Path
+    from Webscraping import CONNECT, USER
+    
+    MYSQL = CONNECT()
+    DROPBOX = USER / r'Dropbox\ん'
+    CASE = r'''
+        case type
+        when 1 then CONCAT('{0}\エラティカ ニ\', path)
+        when 2 then CONCAT('{0}\エラティカ 三\', path)
+        when 3 then CONCAT('{0}\エラティカ 四\', path)
+        end
+        '''.format(DROPBOX).replace('\\', '\\\\')
+    SELECT = f'SELECT {CASE} FROM imageData WHERE NOT ISNULL(path)'
+    DELETE = 'DELETE FROM imageData WHERE path=%s'
+
+    database = set(
+        Path(path) for path, in MYSQL.execute(SELECT, fetch=1)
         )
-    CONNECTION.execute(SELECT.format('path, tags, href, site',))
+    windows = set(DROPBOX.glob('エラティカ */*'))
+    x, y = database - windows, windows - database
+    
+    for num, file in enumerate(y, 1):
+        file.replace(DROPBOX / r'Downloads\Test' / file.name)
+    else:
+        try: print(f'{num} files moved')
+        except: print('0 files moved')
+    
+    for num, file in enumerate(x, 1):
+        MYSQL.execute(DELETE, (file.name,), commit=1)
+    else:
+        try: print(f'{num} records deleted')
+        except: print('0 records deleted')
 
-    for (path, tags, href, site,) in fetch_row():
+def Check_Predictions(sql=False, num=25):
+    
+    from MachineLearning import Model
 
-        progress.next()
-        if site == 'sankaku':
-            page_source = requests.get(f'https://chan.sankakucomplex.com{href}')
-            html = bs4.BeautifulSoup(page_source.content, 'lxml')
-            metadata = set(
-                '_'.join(tag.text.split()[:-2]) for tag in 
-                html.findAll(class_='tag-type-medium')
-                )
-        else:
-            page_source = requests.get(f'https://gelbooru.com/{href}')
-            html = bs4.BeautifulSoup(page_source.content, 'lxml')
-            metadata = set(
-                '_'.join(tag.text.split(' ')[1:-1]) for tag in 
-                html.findAll(class_='tag-type-metadata')
-                )
+    model = Model('deepdanbooru.hdf5')
+
+    if sql:
         
-        tags = ' '.join((metadata & METADATA) - set(tags.split()))
-        if tags:
-            CONNECTION.execute(UPDATE, (f'{tags} ', path), commit=1)
-            CONNECTION.execute(SELECT.format('path, tags, href, site',))
+        from Webscraping import USER, CONNECT
+
+        path = USER / r'Dropbox\ん'
+        
+        TYPE = {
+            'Photograph': 'エラティカ ニ',
+            'Illustration': 'エラティカ 三',
+            'Comic': 'エラティカ 四',
+            }
+        MYSQL = CONNECT()
+        SELECT = f'''
+            SELECT path, tags, type 
+            FROM imageData 
+            WHERE SUBSTR(path, 32, 5) IN ('.jpg', '.png')
+            ORDER BY RAND() LIMIT {num}
+            '''
+
+        for image, tags, type_ in MYSQL.execute(SELECT, fetch=1):
+
+            tags = sorted(tags.split())
+            image = path / TYPE[type_] / image
+            prediction = model.predict(image)
+            similar = set(tags) & set(prediction)
+
+    else:
+
+        import cv2
+        import numpy as np
+        from PIL import Image
+        from random import choices
+
+        glob = list(path.glob('エラティカ *\*jpg'))
+
+        for image in choices(glob, k=num):
+
+            prediction = model.predict(image)
+
+            image_ = np.aray(Image.open(image))
+            image_ = cv2.cvtColor(image_, cv2.COLOR_RGB2BGR)
+            cv2.imshow(prediction, image_)
+            cv2.waitKey(0)
+
+def Copy_Files(files, dest):
+    
+    from pathlib import Path
+
+    dest = Path(dest)
+    paths = [Path(file) for file in files]
+    [
+        (dest / path.name).write_bytes(path.read_bytes()) 
+        for path in paths
+        ]
+
+def Artist_statistics():
+
+    from Webscraping import CONNECT
+
+    MYSQL = CONNECT()
+
+    SELECT = 'SELECT artist FROM imagedata GROUP BY artist HAVING COUNT(artist) > 100 ORDER BY artist'
+    STATS = '''SELECT (
+        SELECT COUNT(*) FROM imagedata 
+        WHERE MATCH(tags, artist) AGAINST(%s IN BOOLEAN MODE) AND stars
+        ) AS TOTAL,
+        (
+        SELECT SUM(stars) FROM imagedata 
+        WHERE MATCH(tags, artist) AGAINST(%s IN BOOLEAN MODE)
+        ) AS STARS
+        '''
+
+    for artist, in MYSQL.execute(SELECT, fetch=1)[1:101]:
+
+        sum, star = MYSQL.execute(STATS, (artist, artist), fetch=1)
+        try: print(f'{artist.strip():<25} (Num: {sum:>4}, Stars: {star:>4}): {star / (sum*5):>4.2%}')
+        except: continue
 
 def Find_symmetric_videos():
 
     from pathlib import Path
     from cv2 import VideoCapture
-    from Webscraping import CONNECTION 
+    from Webscraping import CONNECT
 
     def compare(seq):
         
@@ -195,10 +223,11 @@ def Find_symmetric_videos():
         return any([
             compare(seq[1:]), compare(seq), compare(seq[:-1])
             ])
-
-    SELECT = 'SELECT path FROM imageData WHERE MATCH(tags, artist) AGAINST("animated -audio" IN BOOLEAN MODE) AND type=0'
     
-    for path, in CONNECTION.execute(SELECT, fetch=1):
+    MYSQL = CONNECT()
+    SELECT = 'SELECT path FROM imageData WHERE MATCH(tags, artist) AGAINST("animated -audio" IN BOOLEAN MODE) AND type=0'
+
+    for path, in MYSQL.execute(SELECT, fetch=1):
 
         frames = []
         vidcap = VideoCapture(path)
@@ -283,37 +312,15 @@ def make_stitch():
     stitcher = cv2.Stitcher.create(cv2.Stitcher_SCANS)
     stitcher.setPanoConfidenceThresh(0.1)
     
-    folder = Path(input('Enter path: '))
-    for num, path in enumerate(folder.iterdir()):
-        status, image = stitcher.stitch(get_frames(path))
-        cv2.imwrite(str(test / f'{num:03}'), image)
-
-def Get_Starred():
-
-    from Webscraping import WEBDRIVER, CONNECT
-    import bs4, time
-
-    CONNECTION = CONNECT()
-    DRIVER = WEBDRIVER(0)
-    UPDATE = 'UPDATE imageData SET stars=4 WHERE path=%s AND stars=0'
-    address = '//body/div[1]/div[6]/div/div/div[1]/div/div/main/div/section[3]/div[2]/div/div[1]/div[1]/div[1]/button'
-
-    DRIVER.get('https://www.dropbox.com/h')
-    time.sleep(5)
-    html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
-
-    while starred:=html.findAll(class_='starred-item__content'):
-
-        paths = [(target.text,) for target in starred]
-        CONNECTION.execute(UPDATE, paths, many=1, commit=1)
-        [
-            DRIVER.find(address, click=True) for _ in range(5)
-            ]
+    path = Path(input('Enter path: '))
+    # for num, path in enumerate(folder.iterdir()):
+    status, image = stitcher.stitch(get_frames(path))
+    cv2.imwrite(str(test / f'{num:03}'), image)
 
 Controller()
 # Remove_redundancies()
-# Normalize_database() 
-# Clean_dataset()
+# Normalize_database()
+# Check_Predictions(1)
 # Find_symmetric_videos()
 # make_stitch()
-# Get_Starred()
+# Copy_Files()
