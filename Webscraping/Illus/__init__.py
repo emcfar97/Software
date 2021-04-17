f916957495eb12470ff80200c4c058c5d2e36f3f
Rayos1 <emc1140@hotmail.com>
2021-04-16 19:00:41 -0500

    Added initialize variable

--- Webscraping/Illus/__init__.py
+++ Webscraping/Illus/__init__.py
@@ -2,13 +2,13 @@ import threading
 from . import gelbooru
 from .. import Favorites, sankaku
 
-def start():
+def start(initialize=True):
 
     Favorites.start()
 
     threads = [
-        threading.Thread(target=gelbooru.start),
-        threading.Thread(target=sankaku.start, args=(1,))
+        threading.Thread(target=gelbooru.start, args=(initialize,)),
+        threading.Thread(target=sankaku.start, args=(1, initialize))
         ]
     for thread in threads: thread.start()
     for thread in threads: thread.join()
--- Webscraping/Photos/__init__.py
+++ Webscraping/Photos/__init__.py
@@ -1,16 +1,16 @@
 from .. import sankaku
-from . import flickr, imagefap, posespace, elitebabes, instagram, pinterest, blogspot
+from . import flickr, posespace, elitebabes, instagram, blogspot
 
-def start():
+def start(initialize=True):
 
     import threading 
 
     threads = [
-        threading.Thread(target=flickr.start),
-        threading.Thread(target=elitebabes.start),
-        # threading.Thread(target=instagram.start),
-        # threading.Thread(target=posespace.start)
-        threading.Thread(target=sankaku.start, args=(0,)),
+        threading.Thread(target=flickr.start, args=(initialize,)),
+        threading.Thread(target=elitebabes.start, args=(initialize,)),
+        # threading.Thread(target=instagram.start, args=(initiailze,)),
+        # threading.Thread(target=posespace.start, args=(initiailze,))
+        threading.Thread(target=sankaku.start, args=(0, initialize)),
         ]
     for thread in threads: thread.start()
     for thread in threads: thread.join()
