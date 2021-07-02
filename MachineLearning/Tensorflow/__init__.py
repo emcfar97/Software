fc9a0eb2bc7e27dc5ae2878e422dddda31b78fcd
Rayos1 <emc1140@hotmail.com>
2021-07-02 11:43:44 -0500

    Tensorflow

--- MachineLearning/Tensorflow/__init__.py
+++ MachineLearning/Tensorflow/__init__.py
@@ -1,6 +1,8 @@
-from pathlib import Path
+if __name__ == '__main__': from __init__ import *
+else: from . import *
 
-HOME = Path(__file__).parent
+import tensorflow as tf
+
 MODELS = HOME / 'Models'
 CHCKPNT = HOME / 'Checkpoints'
 LOGS = HOME / 'Logs'
---
+++ MachineLearning/Tensorflow/dataset.py
@@ -0,0 +1,70 @@
+if __name__ == '__main__': from __init__ import *
+else: from . import *
+
+class Custom_Dataset():
+
+    def __init__(self, path):
+
+        self.path = path
+        self.data = tf.data.Dataset.list_files(
+            self.path.glob('*/*'), shuffle=False
+            )
+        self.image_count = self.data.cardinality().numpy()
+        self.data = self.data.shuffle(
+            self.image_count, reshuffle_each_iteration=False
+            )
+        self.classes = np.array(
+            sorted(
+                [item.name for item in self.path.glob('*') 
+                if item.name != 'LICENSE.txt']
+                )
+            )
+
+    def get_training_set(self, split=.2):
+
+        val_size = int(self.image_count * split)
+
+        train_ds = self.data.skip(val_size)
+        val_ds = self.data.take(val_size)
+
+        train_ds = train_ds.map(self.process_path, num_parallel_calls=AUTOTUNE)
+        val_ds = val_ds.map(self.process_path, num_parallel_calls=AUTOTUNE)
+        
+        train_ds = self.configure_for_performance(train_ds)
+        val_ds = self.configure_for_performance(val_ds)
+
+        return train_ds, val_ds
+    
+    def configure_for_performance(self, ds):
+  
+        ds = ds.cache()
+        ds = ds.shuffle(buffer_size=1000)
+        ds = ds.batch(batch_size)
+        ds = ds.prefetch(buffer_size=AUTOTUNE)
+  
+        return ds
+    
+    def get_label(self, file_path):
+
+        # convert the path to a list of path components
+        parts = tf.strings.split(file_path, os.path.sep)
+        # The second to last is the class-directory
+        one_hot = parts[-2] == class_names
+        # Integer encode the label
+        return tf.argmax(one_hot)
+
+    def decode_img(self, img):
+
+        # convert the compressed string to a 3D uint8 tensor
+        img = tf.image.decode_jpeg(img, channels=3)
+        # resize the image to the desired size
+        return tf.image.resize(img, [img_height, img_width])
+
+    def process_path(self, file_path):
+
+        label = get_label(file_path)
+        # load the raw data from the file as a string
+        img = tf.io.read_file(file_path)
+        img = decode_img(img)
+
+        return img, label
