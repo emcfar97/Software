8de8680fb760f9fa1fb047bff467e949668dbef1
Rayos1 <emc1140@hotmail.com>
2021-07-20 19:57:08 -0500

    Prelim code for creat_menu, modied CONNECT's execute

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -4,8 +4,9 @@ from pathlib import Path
 from datetime import date
 from cv2 import VideoCapture
 import mysql.connector as sql
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QCompleter
+from PyQt5.QtWidgets import QCompleter, QMenu, QAction, QActionGroup
 
 class CONNECT:
     
@@ -22,7 +23,7 @@ class CONNECT:
 
         self.transaction = True
 
-        for _ in range(10):
+        for _ in range(5):
             try:
                 if many: self.CURSOR.executemany(statement, arguments)
                 else: self.CURSOR.execute(statement, arguments)
@@ -36,10 +37,11 @@ class CONNECT:
                 if fetch: return self.CURSOR.fetchall()
                 return list()
             
-            except sql.errors.DatabaseError: self.reconnect()
-
+            except (sql.errors.DatabaseError, sql.errors.InterfaceError):
+                
+                self.reconnect()
+        
         self.transaction = False
-        return list()
 
     def rollback(self): self.DATAB.rollback()
 
@@ -85,15 +87,72 @@ def get_frame(path):
     if image is None: return QPixmap()
     return qimage2ndarray.array2qimage(image).rgbSwapped()
 
-def create_menu():
+def create_submenu(parent, name, items, check=None, get_menu=False):
+        
+    if name is None: menu = parent
+    else: menu = QMenu(name, parent)
+    action_group = QActionGroup(menu)
 
-    pass
+    for num, item in enumerate(items):
+        action = QAction(item, menu, checkable=True)
+        if num == check: action.setChecked(True)
+        action_group.triggered.connect(parent.parent().parent().populate)
+        action_group.addAction(action)
+        menu.addAction(action)
 
+    else:
+        if name is not None: parent.addMenu(menu)
+        action_group.setExclusive(True)
+    
+    if get_menu: return action_group, menu
+    return action_group
+
+def remove_redundancies():
+
+    from Webscraping import CONNECT
+
+    MYSQL = CONNECT()  
+    SELECT = 'SELECT path, artist, tags FROM imagedata WHERE NOT ISNULL(path)'
+    UPDATE = 'UPDATE imagedata SET artist=%s, tags=%s WHERE path=%s'
+
+    for (path, artist, tags,) in MYSQL.execute(SELECT, fetch=1):
+
+        artist = f' {" ".join(set(artist.split()))} '
+        tags = f' {" ".join(set(tags.split()))} '
+        MYSQL.execute(UPDATE, (artist, tags, path))
+
+    MYSQL.commit()
+    MYSQL.close()
+
+def update_autocomplete():
+
+    from pathlib import Path
+    from Webscraping import CONNECT
+
+    MYSQL = CONNECT()
+    
+    MYSQL.execute('SET GLOBAL group_concat_max_len=10000000')
+    artist, tags = MYSQL.execute(
+        '''SELECT 
+        GROUP_CONCAT(DISTINCT artist ORDER BY artist SEPARATOR ""), 
+        GROUP_CONCAT(DISTINCT tags ORDER BY tags SEPARATOR "") 
+        FROM imagedata''',
+        fetch=1)[0]
+    text = (
+        ' '.join(sorted(set(artist.split()))), 
+        ' '.join(sorted(set(tags.split())))
+        )
+    text = ('\n'.join(text)).encode('ascii', 'ignore')
+    Path(r'GUI\autocomplete.txt').write_text(text.decode())
+    
+    MYSQL.close()
+
+BATCH = 10000
 ROOT = Path(Path().cwd().drive)
 PATH = ROOT / path.expandvars(r'\Users\$USERNAME\Dropbox\ん')
 parts = ", ".join([f"'{part}'" for part in PATH.parts]).replace('\\', '')
-BASE = f'SELECT full_path(path, {parts}), artist, tags, rating, stars, type, site FROM imageData'
+BASE = f'SELECT full_path(path, {parts}), artist, tags, rating, stars, type, site FROM imagedata'
 COMIC = 'SELECT parent FROM comic WHERE path_=get_name(%s)'
-GESTURE = f'UPDATE imageData SET date_used="{date.today()}" WHERE path=get_name(%s)'
-MODIFY = 'UPDATE imageData SET {} WHERE path=get_name(%s)'
-DELETE = 'DELETE FROM imageData WHERE path=get_name(%s)'
+GESTURE = f'UPDATE imagedata SET date_used="{date.today()}" WHERE path=get_name(%s)'
+MODIFY = 'UPDATE imagedata SET {} WHERE path=get_name(%s)'
+DELETE = 'DELETE FROM imagedata WHERE path=get_name(%s)'
