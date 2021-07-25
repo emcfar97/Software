5c0ea4d7c21521d40fc3f5f7ce5feb98b3576816
Rayos1 <emc1140@hotmail.com>
2021-07-25 18:54:51 -0500

    Converted comics from folder-based to selenium-based

--- Webscraping/comics.py
+++ Webscraping/comics.py
@@ -1,8 +1,7 @@
-import re, send2trash
-from . import USER, CONNECT, INSERT, WEBDRIVER
-from .utils import ARTIST, IncrementalBar, get_name, get_hash, get_tags, generate_tags
+from . import CONNECT, INSERT, SELECT, UPDATE, DELETE, WEBDRIVER
+from .utils import USER, IncrementalBar, save_image, get_hash, get_name, get_tags, generate_tags, bs4, requests, re, ARTIST
 
-path = USER / r'Downloads\Images\Comics'
+SITE = 'nhentai'
 
 def get_artist(text):
 
@@ -13,11 +12,94 @@ def get_artist(text):
 
     return targets.replace('_)', ')')
 
-def start(headless=True):
+def initialize(page='/favorites/?page=1', query=0):
     
+    def next_page(pages):
+
+        try: return pages.find(class_='next').get('href')
+        except AttributeError: return False
+
+    if not query:
+        query = set(MYSQL.execute(SELECT[0], (SITE,), fetch=1))
+        
+    DRIVER.get(f'https://{SITE}.net{page}')
+    html = bs4.BeautifulSoup(DRIVER.page_source(), 'lxml')
+    hrefs = [
+        (href, SITE) for target in 
+        html.findAll('a', class_='cover', href=True)
+        if (href:=target.get('href'),) not in query
+        ]
+        
+    next = next_page(html.find(class_='pagination'))
+    if hrefs and next: return hrefs + initialize(next, query)
+    else: return hrefs
+    
+def page_handler(hrefs):
+
+    if not hrefs: return
+    progress = IncrementalBar(SITE, max=MYSQL.rowcount)
+
+    for href, in hrefs:
+        
+        progress.next()
+        page_source = requests.get(f'https://{SITE}.net{href}')
+        html = bs4.BeautifulSoup(page_source.content, 'lxml')
+        
+        cover = None
+        artists = [
+            artist.get('href').split('/')[-2].replace('-', '_')
+            for artist in html.findAll(href=re.compile('/artist/.+'))
+            ]
+        artists = [
+            ARTIST.get(artist, [artist])[0] for artist in artists
+            ]
+
+        for image in html.findAll('a', class_='gallerythumb'):
+            
+            page_source = requests.get(f'https://{SITE}.net{image.get("href")}')
+            html = bs4.BeautifulSoup(page_source.content, 'lxml')
+            src = html.find(src=re.compile('.+galleries.+')).get('src')
+            
+            name = get_name(src)
+            if cover is None: cover = name
+            if not save_image(name, src): break
+
+            tags, rating, exif = generate_tags(
+                general=get_tags(DRIVER, name, True), 
+                custom=True, rating=True, exif=True
+                )
+            save_image(name, src, exif)
+            hash_ = get_hash(name)
+
+            MYSQL.execute(INSERT[3], (
+                name.name, ' '.join(artists), tags, rating, 
+                3, hash_, src, SITE, href
+                ))
+            MYSQL.execute(INSERT[4], (name.name, cover.name))
+        
+        else: MYSQL.execute(DELETE[0], (href,), commit=1)
+
+    print()
+
+def start(initial=True, headless=True, mode=1):
+    
+    global MYSQL, DRIVER
     MYSQL = CONNECT()
-    DRIVER = WEBDRIVER(headless, None)
+    DRIVER = WEBDRIVER(headless)
     
+    if mode:
+        if initial:
+
+            hrefs = initialize()
+            MYSQL.execute(INSERT[0], hrefs, many=1, commit=1)
+
+        page_handler(MYSQL.execute(SELECT[2], (SITE,), fetch=1))
+        DRIVER.close()
+        return
+
+    import send2trash
+
+    path = USER / r'Downloads\Images\Comics'
     folders = list(path.iterdir())
     if not (length := len(folders)): return
     progress = IncrementalBar('Comic', max=length)
