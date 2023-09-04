f92de885babfb46e7239bf7f3710822acab84bf3
Rayos1 <emc1140@hotmail.com>
2023-09-04 00:32:23 -0500

    Finally figured out command line

--- Webscraping/Favorites/__init__.py
+++ Webscraping/Favorites/__init__.py
@@ -3,12 +3,12 @@ def main(initialize=1, depth=-1):
     import threading, subprocess
     from . import favorites, foundry, furaffinity, twitter
 
-    # process = subprocess.Popen([
-    #     r'python Webscraping\Pixivutil\PixivUtil2.py --start_action 6 --ep 7',
-    #     ])
+    process = subprocess.Popen(
+        r'Webscraping\PixivUtil2\PixivUtil2.exe -s 6 -p y --ep 10 -x'
+        )
     
     threads = [
-        # threading.Thread(target=process.wait),
+        threading.Thread(target=process.wait),
         threading.Thread(target=twitter.main, args=(initialize,)),
         threading.Thread(target=foundry.main, args=(initialize,)),
         threading.Thread(target=furaffinity.main, args=(initialize,)),
