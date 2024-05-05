df6474303540e1db4b771002c7ced1d1126af8a1
Rayos1 <emc1140@hotmail.com>
2024-05-05 18:02:25 -0500

    Tweaks to get_tags, get_redgifs, & save_image

--- Webscraping/utils.py
+++ Webscraping/utils.py
@@ -1,26 +1,28 @@
-import requests, re, tempfile, json, ffmpeg
+import requests, re, tempfile, json, ffmpeg, warnings
 from . import ROOT, USER, TOKEN, get_token
 from math import log
 from io import BytesIO
 from hashlib import md5
 from ffprobe import FFProbe
+from ffprobe.exceptions import FFProbeError
 from imagehash import dhash
 from ast import literal_eval
 from progress.bar import IncrementalBar
 from deepdanbooru_onnx import DeepDanbooru
-from PIL import Image, GifImagePlugin, UnidentifiedImageError, ImageFile
+from PIL import Image, ImageFile, GifImagePlugin, UnidentifiedImageError
 from cv2 import VideoCapture, imencode, cvtColor, COLOR_BGR2RGB, CAP_PROP_FRAME_COUNT, CAP_PROP_POS_FRAMES
 
+warnings.simplefilter('ignore', Image.DecompressionBombWarning)
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 PATH = USER / r'Dropbox\ん'
-RESIZE = [1320, 1000]
-TOKEN = TOKEN
+RESIZE = [1320, 1320]
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0'
     }
 MODEL = DeepDanbooru(
     tags_path=r'MachineLearning\general-tags.txt', threshold=0.475
     )
+EXT = 'jpe?g|png|webp|gif|mp4|webm'
 
 data = json.load(open(r'Webscraping\constants.json', encoding='utf-8'))
 
@@ -32,120 +34,126 @@ ARTIST = data['ARTIST']
 REMOVE = set(data['REMOVE'])
 REPLACE = data['REPLACE']
 
-def save_image(name, image=None):
+def save_image(name, image=None, web_format=True):
     '''Save image to name (with optional exif metadata)'''
 
     try:
         if re.search('jp.*g|png|bmp|webp', name.suffix):
             
             if image:
-                img = Image.open(BytesIO(
-                    requests.get(image, headers=HEADERS).content
-                    ))
-                img.thumbnail(RESIZE)
                 
-            else: img = Image.open(name)    
-            
-            name = name.with_suffix('.webp')
-            img = img.convert('RGBA')
-            img.save(name, 'webp')
-
-        elif re.search('gif|mp4|webm', name.suffix):
-            
-            if 'redgif' in str(image):
+                if web_format:
+                    
+                    webp = name.with_suffix('.webp')
+                    ffmpeg.input(image).output(str(webp), loglevel="error").run(
+                        overwrite_output=True, capture_stdout=True, capture_stderr=True
+                        )
+                    # name = name.replace(webp)
+                    img = Image.open(webp)
+                    img.thumbnail(RESIZE)
+                    img.save(name)
                 
-                name = download_redgif(name, image)
-                
-            elif 'gfycat' in str(image):
-                
-                name = download_gfycat(name, image)
+                else:
+                    img = Image.open(BytesIO(
+                        requests.get(image, headers=HEADERS).content
+                        ))
+                    img.thumbnail(RESIZE)
+                    img.save(name)
                 
             else:
-                    
+                
+                img = Image.open(name)
+                img.save(name)
+        
+            img.close()
+        
+        elif re.search('gif|mp4|webm', name.suffix):
+            
+            if '//redgif' in str(image): image = get_redgif(name.name)
+                
+            if web_format:
+                
+                webm = name.with_suffix('.webm')
+                ffmpeg.input(image).output(str(webm), vcodec='vp9', loglevel="error").run(
+                    overwrite_output=True, capture_stdout=True, capture_stderr=True
+                    )
+                name = name.replace(webm)
+                
+            else:
+                
                 name.write_bytes(
                     requests.get(image, headers=HEADERS).content
                     )
                     
-                if name.suffix != '.webm':
-                    
-                    webm = name.with_suffix('.webm')
-                    
-                    ffmpeg.input(str(name)).output(str(webm), loglevel="quiet").run(overwrite_output=True)
-                    name = name.replace(webm)
-    
     except UnidentifiedImageError: return False
+    
+    except PermissionError: return False
+    
     except OSError as error:
-        if 'trunc' not in error.args:
-            try: img.save(name.with_suffix('.gif'))
-            except: pass
-    except: pass
+        
+        if error.errno == 22:
+            
+            print('\n', image)
+            return False
+        
+        else: raise error
+        # if 'trunc' not in error.args:
+        #     try: img.save(name.with_suffix('.gif'))
+        #     except: pass
+            
+    except ffmpeg.Error as error:
+        
+        if b'-10054' in error.stderr:
+            
+            print('\n', image)
+            return True
+        
+        elif b'25643' in error.stderr:
+            
+            name.write_bytes(requests.get(image).content)
+            return name.exists()
+    
+        print(error.stderr)
+        return False
     
     return name.exists()
 
-def download_redgif(name, url):
+def get_redgif(name):
     "Use redgif API to download a url's contents to the specified name"
     
-    token = TOKEN
-    sess = requests.Session()
-        
-    image_ID = url.split('/')[-1].lower()
-    try:
-        request = sess.get(
-            f'https://api.redgifs.com/v2/gifs/{image_ID}',
-            headers={"Authorization": f"Bearer {token}"}
-            )
-        rawData = request.json()
-        # if 'error' in rawData: return name.parent
-        
-        hd_video_url = rawData['gif']['urls']['hd']
-        
-    except KeyError:
-        token = get_token()
-        request = sess.get(
-            f'https://api.redgifs.com/v2/gifs/{image_ID}',
-            headers={"Authorization": f"Bearer {token}"}
-            )
-        rawData = request.json()
-        hd_video_url = rawData['gif']['urls']['hd']
-    
-    name.write_bytes(sess.get(hd_video_url).content)
-    
-    return name
+    global TOKEN
+    url = f'https://api.redgifs.com/v2/gifs/{name}'
 
-def download_gfycat(name, url):
-    "Use gfycat API to download a url's contents to the specified name"
+    request = requests.get(
+        url, headers={'Authorization': f'Bearer {TOKEN}'}
+        )
+    raw_data = request.json()
     
-    token = TOKEN
-    sess = requests.Session()
-    
-    image_ID = url.split('/')[-1].lower()
-    try:
-        request = sess.get(
-            f'https://api.gfycat.com/v1/gfycats/{image_ID}',
-            headers={"Authorization": f"Bearer {token}"}
-            )
-        rawData = request.json()
-        # if 'error' in rawData: return name.parent
+    if 'error' in raw_data:
         
-        hd_video_url = rawData['gfyItem']['webmUrl']
+        match raw_data['error']['code']:
+            
+            case 'TokenDecodeError':
+            
+                globals()['TOKEN'] = get_token()
+                request = requests.get(
+                    url, headers={'Authorization': f'Bearer {TOKEN}'}
+                    )
+                hd_video_url = request.json()['gif']['urls']['hd']
         
-    except KeyError:
-        token = get_token()
-        request = sess.get(
-            f'https://api.gfycat.com/v1/gfycats/{image_ID}',
-            headers={"Authorization": f"Bearer {token}"}
-            )
-        rawData = request.json()
-        hd_video_url = rawData['gfyItem']['webmUrl']
-    
-    name.write_bytes(sess.get(hd_video_url).content)
-    
-    return name
+            case 'Gone' | 'NotFound':
+            
+                return None
+        
+    else: hd_video_url = raw_data['gif']['urls']['hd']
+        
+    return hd_video_url
 
 def get_name(path, hasher=1):
     '''Return pathname (from optional hash of image)'''
     
-    if isinstance(path, str): suffix = f'.{path.split(".")[-1]}'
+    if isinstance(path, str): 
+        suffix = '.' + re.findall(EXT, path)[0]
     else: suffix = path.suffix
 
     if suffix in ('.jpg', '.jpeg', '.png', '.bmp'): suffix = '.webp'
@@ -167,7 +175,7 @@ def get_name(path, hasher=1):
 
 def get_hash(image, src=False):
     '''Return perceptual hash of image'''
-        
+       
     if src:
         
         try:
@@ -176,7 +184,7 @@ def get_hash(image, src=False):
                 )
         except: return None
     
-    elif re.search('jp.*g|png|webp|gif', image.suffix, re.IGNORECASE): 
+    elif re.search('jp.*g|png|webp|gif', image.suffix, re.IGNORECASE):
         
         try: image = Image.open(image)
         except: return None
@@ -188,6 +196,8 @@ def get_hash(image, src=False):
             image = cvtColor(video_capture, COLOR_BGR2RGB)
             image = Image.fromarray(image)
         except: return None
+    
+    else: raise TypeError
     
     image.thumbnail([32, 32])
     image = image.convert('L')
@@ -230,9 +240,26 @@ def get_tags(path, filter=False):
         data = FFProbe(str(path)).streams
         
         try:
-            if float(data[0].duration) <= 180: tags.add('short_video')
-            elif 180 < float(data[0].duration) <= 600: tags.add('medium_video')
-            elif 600 < float(data[0].duration): tags.add('long_video')
+            video = data[0]
+            
+            if hasattr(video, 'TAG:DURATION'):
+
+                duration = dict(video.__dict__)['TAG:DURATION'].split(':')
+                duration = sum(
+                    float(x) * 60 ** i for i, x in enumerate(reversed(duration))
+                    )
+                
+            elif hasattr(video, 'duration'):
+                
+                try: duration = video.duration_seconds()
+
+                except: duration = float(video.duration)
+
+            if duration <= 5: tags.add('looping_video')
+            elif 5  <  duration <= 180: tags.add('short_video')
+            elif 180 < duration <= 600: tags.add('medium_video')
+            elif 600 < duration: tags.add('long_video')
+            
         except (TypeError, ValueError): pass
         
         if path.suffix in ('.webm', '.mp4'):
@@ -242,17 +269,21 @@ def get_tags(path, filter=False):
                         tags.add('audio')
                         break
             except: pass
-
-            vidcap = VideoCapture(str(path))
-            frame_count = int(vidcap.get(CAP_PROP_FRAME_COUNT))
-            vidcap.release()
+            
+            try: frame_count = video.frames()
+            except FFProbeError:
+                vidcap = VideoCapture(str(path))
+                frame_count = int(vidcap.get(CAP_PROP_FRAME_COUNT))
+                vidcap.release()
         
         elif path.suffix in ('.gif'):
             
             gifcap = GifImagePlugin.GifImageFile(str(path))
             frame_count = gifcap.n_frames
             gifcap.close()
-            
+        
+        if frame_count <= 0: frame_count = video.framerate
+        
         step = 90 * log((frame_count * .002) + 1) + 1
         frames = list(frame_generator(path, temp_dir, round(step)))
 
@@ -265,7 +296,7 @@ def get_tags(path, filter=False):
     
     return ' '.join(tags)
 
-def generate_tags(general, metadata=0, custom=0, artists=[], rating=0):
+def generate_tags(general, metadata=0, custom=0, rating=0):
     
     tags = ['qwd']
 
@@ -286,17 +317,18 @@ def generate_tags(general, metadata=0, custom=0, artists=[], rating=0):
             )
     if rating:
         
-        tags = set(tags + general.split())
+        tags = list(set(tags + general.split()))
         rating = [
             key for key in RATING if evaluate(
                 f' {" ".join(tags)} ', RATING[key]
                 )
             ]
     
-    tags = " ".join(tags).replace('-', '_')
+    tags = " ".join(sorted(tags)).replace('-', '_')
     for key, value in REPLACE.items():
         tags = re.sub(f' {value} ', f' {key} ', tags)
-
+        if re.match('( _)|(_ )|(__)', tags): print(tags, value, key); raise
+    
     return [tags] + rating if rating else tags
 
 def evaluate(tags, pattern):
