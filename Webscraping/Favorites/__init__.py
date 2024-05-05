98bc00cbe73a1aad9168fe2904d89d677818b0c3
Rayos1 <emc1140@hotmail.com>
2024-05-05 17:32:16 -0500

    Added depth arg, changed endpage for Pixiv

--- Webscraping/Favorites/__init__.py
+++ Webscraping/Favorites/__init__.py
@@ -3,17 +3,19 @@ def main(initialize=1, depth=-1):
     import threading, subprocess
     from . import favorites, foundry, furaffinity, twitter
 
-    process = subprocess.Popen(
-        r'Webscraping\PixivUtil2\PixivUtil2.exe -s 6 -p y --ep 10 -x'
-        )
-    
-    threads = [
-        threading.Thread(target=process.wait),
-        threading.Thread(target=twitter.main, args=(initialize,)),
-        threading.Thread(target=foundry.main, args=(initialize,)),
-        threading.Thread(target=furaffinity.main, args=(initialize,)),
-        ]
-    for thread in threads: thread.start()
-    for thread in threads: thread.join()
+    if initialize:
+        
+        process = subprocess.Popen(
+            r'Webscraping\PixivUtil2\PixivUtil2.exe -s 6 --ep 25 -x'
+            )
+        
+        threads = [
+            threading.Thread(target=process.wait),
+            threading.Thread(target=twitter.main, args=(initialize,)),
+            threading.Thread(target=foundry.main, args=(initialize,)),
+            threading.Thread(target=furaffinity.main, args=(initialize,)),
+            ]
+        for thread in threads: thread.start()
+        for thread in threads: thread.join()
 
     favorites.main(initialize, depth=depth)
--- Webscraping/Favorites/__main__.py
+++ Webscraping/Favorites/__main__.py
@@ -3,13 +3,19 @@ from Webscraping.Favorites import main
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog='Favorites', 
+    prog='favorites', 
     )
 parser.add_argument(
     '-i', '--init', type=int,
     help='Initial argument (default 1)',
     default=1
     )
+parser.add_argument(
+    '-d', '--depth', type=int,
+    help='Depth argument (default 0)',
+    default=0
+    )
+
 args = parser.parse_args()
 
-main(args.init)
+main(args.init, args.depth)
